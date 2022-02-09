1441eac2622d5c9c7a86a8bd45cb73eb897d932a
Luxiu123 <klt5466@163.com>
2022-02-09 13:14:20 +0800

    remove: 去除无用注释

--- CommonUtil/View/CommonEncodingView.xaml.cs
+++ CommonUtil/View/CommonEncodingView.xaml.cs
@@ -6,7 +6,7 @@ using System.Windows;
 
 namespace CommonUtil.View {
     public partial class CommonEncodingView : System.Windows.Controls.Page {
-        private static Logger Logger = LogManager.GetCurrentClassLogger();
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
         private readonly Type[] Routers = {
                 typeof(UnicodeEncodingView),
                 typeof(UTF8EncodingView),
@@ -18,8 +18,6 @@ namespace CommonUtil.View {
         public CommonEncodingView() {
             InitializeComponent();
             RouterService = new(ContentFrame, Routers);
-            //RouterService.Navigate(typeof(UnicodeEncodingView));
-            //EncodingNavigationView.SelectionChanged += NavigationSelectionChanged;
         }
 
         /// <summary>
@@ -28,7 +26,6 @@ namespace CommonUtil.View {
         /// <param name="sender"></param>
         /// <param name="args"></param>
         private void NavigationSelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args) {
-            Console.WriteLine("navigation");
             if (args.SelectedItem is FrameworkElement element) {
                 foreach (var item in Routers) {
                     if (item.Name.Contains(element.Name)) {
