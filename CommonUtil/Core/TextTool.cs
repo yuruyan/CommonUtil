3ccb855555dc51b02dfb3a71af680dab6423e25f
Luxiu123 <klt5466@163.com>
2022-02-12 22:11:41 +0800

    feat: 添加文本去重功能

---
+++ CommonUtil/Core/TextTool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonUtil.Core;
+
+public class TextTool {
+    /// <summary>
+    /// 文本去重
+    /// </summary>
+    /// <param name="text"></param>
+    /// <param name="splitSymbol">分隔符</param>
+    /// <param name="mergeSymbol">合并符</param>
+    /// <param name="trim">移除元素首尾空白</param>
+    /// <returns></returns>
+    public static string RemoveDuplicate(string text, string splitSymbol, string mergeSymbol, bool trim = false) {
+        if (!trim) {
+            return string.Join(mergeSymbol, new HashSet<string>(text.Split(splitSymbol)));
+        }
+        return string.Join(mergeSymbol, new HashSet<string>(text.Split(splitSymbol, StringSplitOptions.TrimEntries)));
+    }
+}
+
---
+++ CommonUtil/View/TextTool/RemoveDuplicateView.xaml.cs
@@ -0,0 +1,113 @@
+﻿using CommonUtil.Core;
+using NLog;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CommonUtil.View {
+    public partial class RemoveDuplicateView : Page {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public static readonly DependencyProperty OutputTextProperty = DependencyProperty.Register("OutputText", typeof(string), typeof(RemoveDuplicateView), new PropertyMetadata(""));
+        public static readonly DependencyProperty InputTextProperty = DependencyProperty.Register("InputText", typeof(string), typeof(RemoveDuplicateView), new PropertyMetadata(""));
+        public static readonly DependencyProperty SymbolOptionsProperty = DependencyProperty.Register("SymbolOptions", typeof(List<string>), typeof(RemoveDuplicateView), new PropertyMetadata());
+
+        /// <summary>
+        /// 输入文本
+        /// </summary>
+        public string InputText {
+            get { return (string)GetValue(InputTextProperty); }
+            set { SetValue(InputTextProperty, value); }
+        }
+        /// <summary>
+        /// 输出文本
+        /// </summary>
+        public string OutputText {
+            get { return (string)GetValue(OutputTextProperty); }
+            set { SetValue(OutputTextProperty, value); }
+        }
+        /// <summary>
+        /// 标记选项
+        /// </summary>
+        public List<string> SymbolOptions {
+            get { return (List<string>)GetValue(SymbolOptionsProperty); }
+            set { SetValue(SymbolOptionsProperty, value); }
+        }
+        private Dictionary<string, string> SymbolDict;
+
+        public RemoveDuplicateView() {
+            SymbolDict = new() {
+                { "换行符（⮠  ）", System.Environment.NewLine },
+                { "制表符（→）", "\t" },
+                { "空格（ ）", " " },
+                { "中文逗号（，）", "，" },
+                { "英文逗号（,）", "," },
+            };
+            SymbolOptions = new(SymbolDict.Keys);
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 复制结果
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CopyResultClick(object sender, RoutedEventArgs e) {
+            Clipboard.SetText(OutputText);
+            Widget.MessageBox.Success("已复制");
+        }
+
+        /// <summary>
+        /// 清空输入
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClearInputClick(object sender, RoutedEventArgs e) {
+            InputText = string.Empty;
+        }
+
+        /// <summary>
+        /// 文本去重
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RemoveDuplicateClick(object sender, RoutedEventArgs e) {
+            if (InputText == string.Empty) {
+                Widget.MessageBox.Info("请输入文本");
+                return;
+            }
+            var splitSymbol = GetComboBoxText(SplitSymbolBox);
+            if (splitSymbol == null) {
+                return;
+            }
+            var mergeSymbol = GetComboBoxText(MergeSymbolBox);
+            if (mergeSymbol == null) {
+                return;
+            }
+            OutputText = TextTool.RemoveDuplicate(InputText, splitSymbol, mergeSymbol, TrimWhiteSpaceCheckBox.IsChecked == true);
+        }
+
+        /// <summary>
+        /// 获取 ComboBox 文本
+        /// </summary>
+        /// <param name="comboBox"></param>
+        /// <returns></returns>
+        private string? GetComboBoxText(ComboBox comboBox) {
+            object selectedValue = comboBox.SelectedValue;
+            string text = string.Empty;
+            // 非用户输入
+            if (selectedValue != null) {
+                if (selectedValue is string t) {
+                    text = SymbolDict[t];
+                }
+            } else {
+                if (string.IsNullOrEmpty(comboBox.Text)) {
+                    Widget.MessageBox.Error("去重分隔符不能为空");
+                    return null;
+                }
+                text = comboBox.Text;
+            }
+            return text;
+        }
+    }
+}
