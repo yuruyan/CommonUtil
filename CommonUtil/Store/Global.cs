f0c1bc0efa90268fb7fab945f7bc8f89cc838c53
Luxiu123 <klt5466@163.com>
2022-02-14 14:00:19 +0800

    feat: 添加二维码生成功能

--- CommonUtil/Store/Global.cs
+++ CommonUtil/Store/Global.cs
@@ -21,6 +21,7 @@ namespace CommonUtil.Store {
             new() { Name = "顺序文本生成", ImagePath = ImagePath + "order.svg", ClassType = typeof(OrdinalTextGeneratorView) },
             new() { Name = "文件内容搜索", ImagePath = ImagePath + "FileSearch.svg", ClassType = typeof(KeywordFinderView) },
             new() { Name = "文本工具", ImagePath = ImagePath + "TextTool.svg", ClassType = typeof(TextToolView) },
+            new() { Name = "二维码工具", ImagePath = ImagePath + "qrcode.svg", ClassType = typeof(QRCodeToolView) },
         };
     }
 
