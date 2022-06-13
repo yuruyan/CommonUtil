441c156931a120b666477f20f61c84633c47cb59
Luxiu123 <klt5466@163.com>
2022-06-13 22:32:31 +0800

    feat: 添加 SimpleFileSystemServer 功能

---
+++ SimpleFileSystemServer/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SimpleFileSystemServer.Service;
+
+namespace SimpleFileSystemServer.Controllers;
+
+[Route("")]
+public class HomeController : Controller {
+    private readonly FileService FileService = new();
+
+    [HttpGet("/path")]
+    [HttpGet("")]
+    public ActionResult Index([FromQuery()] string path = "/") {
+        path = "/" + (path ?? "").Trim('\\', '/');
+        // 非法访问
+        if (!FileService.CheckPathRange(path)) {
+            throw new FileNotFoundException(path);
+        }
+        ViewData["data"] = JsonConvert.SerializeObject(new {
+            currentPath = path,
+            files = FileService.ListFiles(path)
+        });
+        return View("/views/index.cshtml");
+    }
+
+    [HttpGet("/download")]
+    public FileResult Donwload([FromQuery()] string path) {
+        path = (path ?? "").Trim('\\', '/');
+        // 非法访问
+        if (!FileService.CheckPathRange(path)) {
+            throw new FileNotFoundException(path);
+        }
+        var filePath = Path.Combine(Global.WorkingDirectory, path);
+        return File(new FileStream(filePath, FileMode.Open, FileAccess.Read), "application/octet-stream", Path.GetFileName(filePath));
+    }
+}
---
+++ SimpleFileSystemServer/Global.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleFileSystemServer;
+
+public class Global {
+    public static string WorkingDirectory { get; private set; }
+
+    public Global(IConfiguration configuration) {
+        if (WorkingDirectory == null) {
+            WorkingDirectory = configuration["dir"];
+        }
+    }
+}
