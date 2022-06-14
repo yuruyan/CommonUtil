e0b09ea19e19939086b611f2ae28b57ba6f2790d
Luxiu123 <klt5466@163.com>
2022-06-14 22:18:17 +0800

    feat: 完成 SimpleFileSystemServer 功能

--- CommonUtil/Core/SimpleFileSystemServer.cs
+++ CommonUtil/Core/SimpleFileSystemServer.cs
@@ -6,19 +6,46 @@ using Flurl.Http;
 using System.Diagnostics;
 using System.Windows;
 using System.Threading;
+using NLog;
 
 namespace CommonUtil.Core;
 
 public class SimpleFileSystemServer {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
     /// <summary>
     /// Server App 路径
     /// </summary>
-    public static readonly string AppPath = "";
+    public static readonly string AppPath = "SimpleFileSystemServer.exe";
     public int Port { get; }
     public string WordkingDirectory { get; }
-
+    /// <summary>
+    /// 服务器停止事件
+    /// </summary>
+    public event EventHandler Stopped;
+    /// <summary>
+    /// 服务器启动事件
+    /// </summary>
+    public event EventHandler Started;
+    /// <summary>
+    /// 服务器进程
+    /// </summary>
     private Process? ServerProcess;
-    private System.Timers.Timer HeartBeatTimer;
+    /// <summary>
+    /// 检测服务器是否启动定时器
+    /// </summary>
+    private readonly System.Timers.Timer CheckStartedTimer;
+    /// <summary>
+    /// 心跳机制定时器，检测服务器是否宕机
+    /// </summary>
+    private readonly System.Timers.Timer HeartBeatTimer;
+    /// <summary>
+    /// 最长未响应次数，超过此值则认为服务器已经关闭
+    /// </summary>
+    private static readonly byte MaxNoResponseTimes = 16;
+    /// <summary>
+    /// 当前未响应次数
+    /// </summary>
+    private static byte NoResponseTimes;
     /// <summary>
     /// 心跳间隔时间
     /// </summary>
@@ -26,58 +53,92 @@ public class SimpleFileSystemServer {
     /// <summary>
     /// 进程是否启动
     /// </summary>
-    private bool IsStarted;
+    public bool IsStarted { get; private set; }
 
     /// <summary>
     /// 构造函数
     /// </summary>
     /// <param name="port">端口号</param>
-    /// <param name="dir">工作目录</param>
+    /// <param name="dir">分享目录</param>
     public SimpleFileSystemServer(int port, string dir) {
         Port = port;
         WordkingDirectory = dir;
-        #region 初始化 HeartBeatTimer
-        HeartBeatTimer = new(HeartBeatInterval);
-        HeartBeatTimer.Elapsed += async (s, e) => {
+        #region 初始化 CheckStartedTimer、HeartBeatTimer
+        CheckStartedTimer = new(HeartBeatInterval);
+        CheckStartedTimer.Elapsed += async (s, e) => {
             GeneralResponse? resp = null;
-            resp = await CommonUtils.TryAsync(() => $"http://localhost:{Port}".GetJsonAsync<GeneralResponse>());
+            resp = await CommonUtils.TryAsync(() => $"http://localhost:{Port}/heartbeat".GetJsonAsync<GeneralResponse>());
             // 启动成功
             if (resp != null && resp.code == 200) {
                 IsStarted = true;
-                HeartBeatTimer.Stop();
+                CheckStartedTimer.Stop();
+            }
+        };
+        HeartBeatTimer = new(HeartBeatInterval);
+        HeartBeatTimer.Elapsed += async (s, e) => {
+            GeneralResponse? resp = null;
+            resp = await CommonUtils.TryAsync(() => $"http://localhost:{Port}/heartbeat".GetJsonAsync<GeneralResponse>());
+            // 成功
+            if (resp != null && resp.code == 200) {
+                // 清零
+                NoResponseTimes = 0;
+            } else {
+                NoResponseTimes++;
+                Logger.Debug($"HeartBeat {nameof(SimpleFileSystemServer)} retries {NoResponseTimes} times");
+            }
+            // 超过最长未响应时长
+            if (NoResponseTimes >= MaxNoResponseTimes) {
+                Logger.Info($"HeartBeat {nameof(SimpleFileSystemServer)} retries timeout");
+                Stop();
             }
         };
         #endregion
         // 主进程结束，自动结束子进程
         Application.Current.Exit += (s, e) => Stop();
+        // 设置启动、退出事件监听程序
+        Started += (s, e) => {
+            HeartBeatTimer.Start();
+            Logger.Debug($"{nameof(SimpleFileSystemServer)} has started");
+        };
+        Stopped += (s, e) => {
+            HeartBeatTimer.Stop();
+            Logger.Debug($"{nameof(SimpleFileSystemServer)} has stopped");
+        };
     }
 
     /// <summary>
     /// 异步启动文件服务器
     /// </summary>
     /// <param name="waitTime">ms, 等待多长时间未启动则取消</param>
-    public Task<bool> StartAsync(int waitTime = 5000) {
+    public async Task<bool> StartAsync(int waitTime = 5000) {
         // 已经有服务器在运行
         if (ServerProcess is not null) {
-            return Task.Run(() => true);
+            return true;
         }
         ServerProcess = Process.Start(AppPath, $" --urls=http://*:{Port} --dir=\"{WordkingDirectory}\"");
-        HeartBeatTimer.Start();
-        return Task.Run(() => {
+        if (ServerProcess is null) {
+            return false;
+        }
+        CheckStartedTimer.Start();
+        return await Task.Run(() => {
             var beginTime = DateTime.Now;
             // 判断是否启动成功
             while (true) {
                 // 超时结束
                 if ((DateTime.Now - beginTime).TotalMilliseconds > waitTime) {
+                    Logger.Info($"Start {nameof(SimpleFileSystemServer)} timeout");
                     Stop();
                     return false;
                 }
                 // 进程正常启动
                 if (IsStarted) {
+                    Started?.Invoke(this, EventArgs.Empty);
                     return true;
                 }
                 // 进程终止
                 if (ServerProcess.HasExited) {
+                    Logger.Info($"{nameof(SimpleFileSystemServer)} exit with code {ServerProcess.ExitCode}");
+                    Stop();
                     return false;
                 }
                 Thread.Sleep(HeartBeatInterval);
@@ -86,11 +147,16 @@ public class SimpleFileSystemServer {
     }
 
     /// <summary>
-    /// 停止服务器
+    /// 停止服务器，ServerProcess 设置为 null
     /// </summary>
     public void Stop() {
-        ServerProcess?.Kill();
+        if (ServerProcess is null) {
+            return;
+        }
+        CommonUtils.Try(() => ServerProcess?.Kill());
         ServerProcess = null;
+        CheckStartedTimer.Stop();
         IsStarted = false;
+        Stopped?.Invoke(this, EventArgs.Empty);
     }
 }
---
+++ CommonUtil/View/SimpleFileSystemServerView.xaml.cs
@@ -0,0 +1,165 @@
+﻿using CommonUITools.Utils;
+using CommonUtil.Core;
+using NLog;
+using Ookii.Dialogs.Wpf;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CommonUtil.View {
+    public partial class SimpleFileSystemServerView : Page {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        public static readonly DependencyProperty SharingDirectoryProperty = DependencyProperty.Register("SharingDirectory", typeof(string), typeof(SimpleFileSystemServerView), new PropertyMetadata(""));
+        public static readonly DependencyProperty IsServerStartedProperty = DependencyProperty.Register("IsServerStarted", typeof(bool), typeof(SimpleFileSystemServerView), new PropertyMetadata(false));
+        public static readonly DependencyProperty ServerPortProperty = DependencyProperty.Register("ServerPort", typeof(int), typeof(SimpleFileSystemServerView), new PropertyMetadata(3000));
+
+        /// <summary>
+        /// 分享文件目录
+        /// </summary>
+        public string SharingDirectory {
+            get { return (string)GetValue(SharingDirectoryProperty); }
+            set { SetValue(SharingDirectoryProperty, value); }
+        }
+        private CommonUtil.Core.SimpleFileSystemServer? SimpleFileSystemServer;
+        /// <summary>
+        /// 服务器是否启动
+        /// </summary>
+        public bool IsServerStarted {
+            get { return (bool)GetValue(IsServerStartedProperty); }
+            set { SetValue(IsServerStartedProperty, value); }
+        }
+        /// <summary>
+        /// 服务器端口
+        /// </summary>
+        public int ServerPort {
+            get { return (int)GetValue(ServerPortProperty); }
+            set { SetValue(ServerPortProperty, value); }
+        }
+
+        public SimpleFileSystemServerView() {
+            InitializeComponent();
+            SharingDirectory = Directory.GetCurrentDirectory();
+            DependencyPropertyDescriptor.FromProperty(IsServerStartedProperty, typeof(SimpleFileSystemServerView)).AddValueChanged(this, ServerStateChangedHandler);
+        }
+
+        /// <summary>
+        /// 服务器状态变化 Handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ServerStateChangedHandler(object? sender, EventArgs e) {
+            if (IsServerStarted) {
+                // 监听停止状态
+                SimpleFileSystemServer.Stopped += (s, e) => {
+                    Dispatcher.Invoke(() => IsServerStarted = false);
+                };
+            }
+        }
+
+        /// <summary>
+        /// 选择分享文件目录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectSharingDirectoryClick(object sender, RoutedEventArgs e) {
+            e.Handled = true;
+            var dialog = new VistaFolderBrowserDialog {
+                Description = "选择分享目录",
+                UseDescriptionForTitle = true
+            };
+            if (dialog.ShowDialog(Application.Current.MainWindow) == true) {
+                SharingDirectory = dialog.SelectedPath;
+            }
+        }
+
+        /// <summary>
+        /// 点击目录文字，打开所在目录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenSharingDirectoryMouseUp(object sender, MouseButtonEventArgs e) {
+            e.Handled = true;
+            if (sender is TextBlock element) {
+                UIUtils.OpenFileInDirectoryAsync(element.Text);
+            }
+        }
+
+        /// <summary>
+        /// 切换服务器状态
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToggleServerStateClickHandler(object sender, RoutedEventArgs e) {
+            e.Handled = true;
+            #region 检查是否选择分享目录
+            if (string.IsNullOrEmpty(SharingDirectory)) {
+                CommonUITools.Widget.MessageBox.Info("未选择分享目录！");
+                return;
+            }
+            #endregion
+            // 关闭服务器
+            if (IsServerStarted) {
+                StopServer();
+                return;
+            }
+            // 开启服务器
+            ThrottleUtils.ThrottleAsync(ToggleServerStateClickHandler, async () => {
+                bool state = await StartServerAsync();
+                IsServerStarted = state;
+                // 开启成功
+                if (state) {
+                    CommonUITools.Widget.MessageBox.Success("服务器启动成功");
+                    return;
+                }
+                StopServer();
+                CommonUITools.Widget.MessageBox.Error("服务器启动失败");
+            });
+        }
+
+        /// <summary>
+        /// 主动停止服务器
+        /// </summary>
+        private void StopServer() {
+            IsServerStarted = false;
+            SimpleFileSystemServer?.Stop();
+        }
+
+        /// <summary>
+        /// 异步启动服务器
+        /// </summary>
+        /// <returns>成功返回 true，失败返回 false</returns>
+        private async Task<bool> StartServerAsync() {
+            int port = CommonUtils.Try(GetFreePort, 0);
+            // 端口不足
+            if (port == 0) {
+                CommonUITools.Widget.MessageBox.Error("端口不足");
+                return false;
+            }
+            ServerPort = port;
+            SimpleFileSystemServer = new(port, SharingDirectory);
+            return await SimpleFileSystemServer.StartAsync();
+        }
+
+        /// <summary>
+        /// 获取未占用端口
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception">端口不足，找不到合适端口</exception>
+        private int GetFreePort() {
+            int port = ServerPort;
+            // 获取占用端口
+            var inUserPorts = NetworkUtils.GetInUsePorts().ToHashSet();
+            for (; port < ServerPort + 7000 && port < 65536; port++) {
+                if (!inUserPorts.Contains(port)) {
+                    return port;
+                }
+            }
+            throw new Exception("端口不足");
+        }
+    }
+}
