74b1a8a24dbb5ae2a42d266ffab5e07d6901a48b
Luxiu123 <klt5466@163.com>
2022-07-03 14:48:10 +0800

    refactor: 移除文件 DownloaderView

---
+++ CommonUtil/View/Downloader/DownloaderView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using CommonUITools.Route;
+using CommonUITools.Utils;
+using CommonUtil.Core;
+using Microsoft.Win32;
+using ModernWpf.Controls;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CommonUtil.View;
+
+public partial class DownloaderView : System.Windows.Controls.Page {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger(); private readonly RouterService RouterService;
+
+    private readonly IDictionary<string, Type> NavigationDict = new Dictionary<string, Type>() {
+        //{"1", typeof(DownloadView) },
+        //{"2", typeof(UploadView) },
+    };
+
+    public DownloaderView() {
+        InitializeComponent();
+    }
+
+    /// <summary>
+    /// 导航改变
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    private void NavigationSelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args) {
+        if (args.SelectedItem is NavigationViewItem viewItem) {
+            //RouterService.Navigate(
+            //    NavigationDict[CommonUtils.NullCheck(viewItem.Tag.ToString())]
+            //);
+        }
+    }
+
+    private void DownloadTaskMouseUpHandler(object sender, MouseButtonEventArgs e) {
+        Logger.Debug("start download task");
+    }
+}
