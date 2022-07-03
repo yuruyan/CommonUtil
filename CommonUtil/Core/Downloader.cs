e1f313ad5ab6f018bd393c9c1797dd5f8a18cc13
Luxiu123 <klt5466@163.com>
2022-07-03 14:16:17 +0800

    mod: 完善 Downloader

--- CommonUtil/Core/Downloader.cs
+++ CommonUtil/Core/Downloader.cs
@@ -1,10 +1,110 @@
-﻿using System;
+﻿using CommonUITools.Utils;
+using CommonUtil.Model;
+using Downloader;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CommonUtil.Core;
 
 public class Downloader {
+    /// <summary>
+    /// 下载配置
+    /// </summary>
+    private static readonly DownloadConfiguration DownloadConfiguration = new() { OnTheFlyDownload = false };
+    /// <summary>
+    /// 下载任务列表 dict，用于更新，与 DownloadTaskInfoList 同步更新
+    /// </summary>
+    public static readonly IDictionary<DownloadService, DownloadTask> DownloadTaskInfoDict = new Dictionary<DownloadService, DownloadTask>();
+    /// <summary>
+    /// 更新进度视图间隔时间
+    /// </summary>
+    public const short UpdateProcessInterval = 500;
+
+    public static event EventHandler<DownloadTask>? DownloadCompleted;
+    public static event EventHandler<DownloadTask>? DownloadFailed;
+
+    /// <summary>
+    /// 下载文件
+    /// </summary>
+    /// <param name="url"></param>
+    /// <param name="directory"></param>
+    /// <returns></returns>
+    public static DownloadTask Download(string url, DirectoryInfo directory) {
+        var downloader = new DownloadService(DownloadConfiguration);
+        downloader.DownloadStarted += DownloadStartedHandler;
+        downloader.DownloadProgressChanged += DownloadProgressChangedHandler;
+        downloader.DownloadFileCompleted += DownloadFileCompletedHandler;
+        downloader.DownloadFileTaskAsync(url, directory);
+        return new DownloadTask(url) {
+            FileName = new Uri(url).Segments.LastOrDefault() ?? "未知文件名"
+        };
+    }
+
+    /// <summary>
+    /// 下载文件开始
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private static void DownloadStartedHandler(object? sender, DownloadStartedEventArgs e) {
+        if (sender is DownloadService service) {
+            var taskInfo = DownloadTaskInfoDict[service];
+            UIUtils.RunOnUIThread(() => {
+                taskInfo.TotalSize = e.TotalBytesToReceive;
+                taskInfo.FileName = e.FileName;
+            });
+        }
+    }
+
+    /// <summary>
+    /// 下载完成事件
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private static void DownloadFileCompletedHandler(object? sender, AsyncCompletedEventArgs e) {
+        if (sender is DownloadService service) {
+            var taskInfo = DownloadTaskInfoDict[service];
+            // 更新视图
+            UIUtils.RunOnUIThread(() => {
+                taskInfo.LastUpdateTime = DateTime.Now;
+                taskInfo.DownloadedSize = taskInfo.TotalSize;
+                taskInfo.Process = 100;
+                taskInfo.FinishTime = DateTime.Now;
+                taskInfo.IsFinished = true;
+                // 从下载列表中移除
+                DownloadTaskInfoDict.Remove(service);
+            });
+            // 下载失败
+            if (e.Error is null) {
+                DownloadFailed?.Invoke(null, taskInfo);
+            } else {
+                DownloadCompleted?.Invoke(null, taskInfo);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 下载进度事件
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private static void DownloadProgressChangedHandler(object? sender, DownloadProgressChangedEventArgs e) {
+        if (sender is DownloadService service) {
+            var taskInfo = DownloadTaskInfoDict[service];
+            // 未到更新时间
+            if ((DateTime.Now - taskInfo.LastUpdateTime).TotalMilliseconds <= UpdateProcessInterval) {
+                return;
+            }
+            // 更新视图
+            UIUtils.RunOnUIThread(() => {
+                taskInfo.LastUpdateTime = DateTime.Now;
+                taskInfo.DownloadedSize = e.ReceivedBytesSize;
+                taskInfo.DownloadSpeed = e.BytesPerSecondSpeed;
+                taskInfo.Process = (byte)e.ProgressPercentage;
+            });
+        }
+    }
+
 }
---
+++ CommonUtil/Model/DownloadTask.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows;
+
+namespace CommonUtil.Model;
+
+/// <summary>
+/// 下载信息
+/// </summary>
+public class DownloadTask : DependencyObject {
+    public DownloadTask(string url, string filename = "") {
+        FileName = filename;
+        Url = url;
+    }
+
+    /// <summary>
+    /// 下载文件 url
+    /// </summary>
+    public string Url { get; }
+    /// <summary>
+    /// 文件名
+    /// </summary>
+    public string FileName {
+        get { return (string)GetValue(FileNameProperty); }
+        set { SetValue(FileNameProperty, value); }
+    }
+    /// <summary>
+    /// 上次更新时间
+    /// </summary>
+    public DateTime LastUpdateTime { get; set; } = DateTime.Now;
+    /// <summary>
+    /// 开始时间
+    /// </summary>
+    public DateTime BeginTime { get; } = DateTime.Now;
+    /// <summary>
+    /// 结束时间
+    /// </summary>
+    public DateTime FinishTime { get; set; } = DateTime.Now;
+    /// <summary>
+    /// 下载速度，byte / s
+    /// </summary>
+    public double DownloadSpeed {
+        get { return (double)GetValue(DownloadSpeedProperty); }
+        set { SetValue(DownloadSpeedProperty, value); }
+    }
+    /// <summary>
+    /// 文件总大小 byte
+    /// </summary>
+    public long TotalSize {
+        get { return (long)GetValue(TotalSizeProperty); }
+        set { SetValue(TotalSizeProperty, value); }
+    }
+    /// <summary>
+    /// 已下载大小 byte
+    /// </summary>
+    public long DownloadedSize {
+        get { return (long)GetValue(DownloadedSizeProperty); }
+        set { SetValue(DownloadedSizeProperty, value); }
+    }
+    /// <summary>
+    /// 是否暂停
+    /// </summary>
+    public bool IsPaused {
+        get { return (bool)GetValue(IsPausedProperty); }
+        set { SetValue(IsPausedProperty, value); }
+    }
+    /// <summary>
+    /// 是否结束
+    /// </summary>
+    public bool IsFinished { get; set; }
+    /// <summary>
+    /// 进度 [0-100]
+    /// </summary>
+    public byte Process {
+        get { return (byte)GetValue(ProcessProperty); }
+        set { SetValue(ProcessProperty, value); }
+    }
+
+    public static readonly DependencyProperty FileNameProperty = DependencyProperty.Register("FileName", typeof(string), typeof(DownloadTask), new PropertyMetadata(string.Empty));
+    public static readonly DependencyProperty ProcessProperty = DependencyProperty.Register("Process", typeof(byte), typeof(DownloadTask), new PropertyMetadata((byte)0));
+    public static readonly DependencyProperty DownloadSpeedProperty = DependencyProperty.Register("DownloadSpeed", typeof(double), typeof(DownloadTask), new PropertyMetadata(0.0));
+    public static readonly DependencyProperty TotalSizeProperty = DependencyProperty.Register("TotalSize", typeof(long), typeof(DownloadTask), new PropertyMetadata(0L));
+    public static readonly DependencyProperty DownloadedSizeProperty = DependencyProperty.Register("DownloadedSize", typeof(long), typeof(DownloadTask), new PropertyMetadata(0L));
+    public static readonly DependencyProperty IsPausedProperty = DependencyProperty.Register("IsPaused", typeof(bool), typeof(DownloadTask), new PropertyMetadata(false));
+}
