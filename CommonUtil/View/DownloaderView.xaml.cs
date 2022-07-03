040989027f4116e1cc36d39f090da781dda2e444
Luxiu123 <klt5466@163.com>
2022-07-03 13:06:50 +0800

    mod: 添加 DownloaderView

---
+++ CommonUtil/View/DownloaderView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using CommonUtil.Core;
+using Microsoft.Win32;
+using NLog;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CommonUtil.View;
+
+public partial class DownloaderView : Page {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    public DownloaderView() {
+        InitializeComponent();
+    }
+
+}
