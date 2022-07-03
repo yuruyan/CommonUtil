250b82c42ab0e7932326aac114c67368eb13a673
Luxiu123 <klt5466@163.com>
2022-07-03 20:13:18 +0800

    feat: 添加下载记录个数显示功能

--- CommonUtil/View/Downloader/DownloaderView.xaml.cs
+++ CommonUtil/View/Downloader/DownloaderView.xaml.cs
@@ -5,7 +5,9 @@ using ModernWpf.Controls;
 using NLog;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows;
 using System.Windows.Input;
 using MessageBox = CommonUITools.Widget.MessageBox;
 
@@ -13,6 +15,9 @@ namespace CommonUtil.View;
 
 public partial class DownloaderView : System.Windows.Controls.Page {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+    public static readonly DependencyProperty DownloadingTaskListProperty = DependencyProperty.Register("DownloadingTaskList", typeof(ObservableCollection<DownloadTask>), typeof(DownloaderView), new PropertyMetadata());
+    public static readonly DependencyProperty DownloadedTaskListProperty = DependencyProperty.Register("DownloadedTaskList", typeof(ObservableCollection<DownloadTask>), typeof(DownloaderView), new PropertyMetadata());
+
     private readonly RouterService RouterService;
     private readonly IDictionary<string, Type> NavigationDict = new Dictionary<string, Type>() {
         {"1", typeof(DownloadingView) },
@@ -25,6 +30,20 @@ public partial class DownloaderView : System.Windows.Controls.Page {
     private readonly DownloadingView DownloadingView;
     private readonly DownloadedView DownloadedView;
     private readonly Core.Downloader Downloader = new();
+    /// <summary>
+    /// 下载任务列表，引用 DownloadingView
+    /// </summary>
+    public ObservableCollection<DownloadTask> DownloadingTaskList {
+        get { return (ObservableCollection<DownloadTask>)GetValue(DownloadingTaskListProperty); }
+        private set { SetValue(DownloadingTaskListProperty, value); }
+    }
+    /// <summary>
+    /// 下载任务列表，引用 DownloadedView
+    /// </summary>
+    public ObservableCollection<DownloadTask> DownloadedTaskList {
+        get { return (ObservableCollection<DownloadTask>)GetValue(DownloadedTaskListProperty); }
+        private set { SetValue(DownloadedTaskListProperty, value); }
+    }
 
     public DownloaderView() {
         InitializeComponent();
@@ -32,6 +51,8 @@ public partial class DownloaderView : System.Windows.Controls.Page {
         // 显式初始化
         DownloadingView = (DownloadingView)RouterService.GetInstance(typeof(DownloadingView));
         DownloadedView = (DownloadedView)RouterService.GetInstance(typeof(DownloadedView));
+        DownloadingTaskList = DownloadingView.DownloadTaskList;
+        DownloadedTaskList = DownloadedView.DownloadTaskList;
         Downloader.DownloadCompleted += DownloadCompletedHandler;
         Downloader.DownloadFailed += DownloadFailedHandler;
     }
