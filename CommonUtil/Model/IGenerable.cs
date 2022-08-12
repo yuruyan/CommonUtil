9005d8c7f3a613cc99d5b59dd60a29e3d2b11f57
Luxiu123 <klt5466@163.com>
2022-08-12 16:02:39 +0800

    mod: 增加 IGenerable 泛型

--- CommonUtil/Model/IGenerable.cs
+++ CommonUtil/Model/IGenerable.cs
@@ -5,3 +5,7 @@ namespace CommonUtil.Model;
 internal interface IGenerable<out T> {
     public IEnumerable<T> Generate();
 }
+
+internal interface IGenerable<In, out Out> {
+    public IEnumerable<Out> Generate(In arg);
+}
