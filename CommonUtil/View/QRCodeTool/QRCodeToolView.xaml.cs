dad558d0e85fdc0aa8a9bcdb24bfa7bb340da937
Luxiu123 <klt5466@163.com>
2022-08-13 19:20:44 +0800

    refactor: 重构 SimpleStackPanel

--- CommonUtil/View/QRCodeTool/QRCodeToolView.xaml.cs
+++ CommonUtil/View/QRCodeTool/QRCodeToolView.xaml.cs
@@ -18,7 +18,6 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using static QRCoder.PayloadGenerator;
 using MessageBox = CommonUITools.Widget.MessageBox;
 
 namespace CommonUtil.View;
--- SimpleFileSystemServerTests/Service/FileServiceTests.cs
+++ SimpleFileSystemServerTests/Service/FileServiceTests.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SimpleFileSystemServer.Service;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleFileSystemServer.Service.Tests {
     [TestClass()]
