5c63fa57daff81c3eb5bbaee5154710e510f7f5d
Luxiu123 <klt5466@163.com>
2022-11-02 20:53:52 +0800

    feat: 添加更多随机选项功能

--- CommonUtil/Core/RandomGenerator.cs
+++ CommonUtil/Core/RandomGenerator.cs
@@ -6,6 +6,7 @@ using System.IO.Compression;
 using System.IO;
 using System.Linq;
 using System.Text;
+using CommonUITools.Utils;
 
 namespace CommonUtil.Core;
 
@@ -32,6 +33,8 @@ public class RandomGenerator {
     private const string ChineseAncientNameEntryName = "ChineseAncientNames.txt";
     private const string EnglishNameEntryName = "EnglishNames.txt";
     private const string EnglishChineseNameEntryName = "EnglishChineseNames.txt";
+    private const string EnglishWordsEntryName = "EnglishWords.txt";
+    private const string ChineseWordsEntryName = "ChineseWords.txt";
     /// <summary>
     /// 数据源
     /// </summary>
@@ -43,7 +46,15 @@ public class RandomGenerator {
         {ChineseAncientNameEntryName, new () },
         {EnglishNameEntryName, new () },
         {EnglishChineseNameEntryName, new () },
+        {EnglishWordsEntryName, new () },
+        {ChineseWordsEntryName, new () },
     };
+
+    private static readonly IRandomizerString RandomizerEmailAddress = RandomizerFactory.GetRandomizer(new FieldOptionsEmailAddress());
+    private static readonly IRandomizerGuid RandomizerGuid = RandomizerFactory.GetRandomizer(new FieldOptionsGuid());
+    private const int IPV4AddressFragment = 256;
+    private const int IPV6AddressFragment = 65536;
+    private const int MACAddressFragment = 256;
 
     /// <summary>
     /// 生成随机数
@@ -155,6 +166,101 @@ public class RandomGenerator {
     }
 
     /// <summary>
+    /// 生成随机 DateTime
+    /// </summary>
+    /// <param name="minDateTime"></param>
+    /// <param name="maxDateTime">不包括</param>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    public static DateTime[] GenerateRandomDateTime(DateTime minDateTime, DateTime maxDateTime, uint count) {
+        long minMills = CommonUtils.ConvertToTimestamp(minDateTime);
+        long maxMills = CommonUtils.ConvertToTimestamp(maxDateTime);
+        long gap = maxMills - minMills;
+        var data = new DateTime[count];
+        for (int i = 0; i < count; i++) {
+            data[i] = minDateTime.AddMilliseconds(Random.Shared.NextInt64(gap));
+        }
+        return data;
+    }
+
+    /// <summary>
+    /// 生成随机邮箱地址
+    /// </summary>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    public static string[] GenerateRandomEmailAddresses(uint count) {
+        var data = new string[count];
+        for (int i = 0; i < count; i++) {
+            data[i] = RandomizerEmailAddress.Generate() ?? string.Empty;
+        }
+        return data;
+    }
+
+    /// <summary>
+    /// 生成随机 guid
+    /// </summary>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    public static string[] GenerateRandomGuids(uint count) {
+        var data = new string[count];
+        for (int i = 0; i < count; i++) {
+            data[i] = RandomizerGuid.Generate()?.ToString() ?? string.Empty;
+        }
+        return data;
+    }
+
+    /// <summary>
+    /// 生成随机 ipv4 地址
+    /// </summary>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    public static string[] GenerateRandomIPV4Addresses(uint count) {
+        var data = new string[count];
+        for (int i = 0; i < count; i++) {
+            data[i] = $"{Random.Shared.Next(IPV4AddressFragment)}.{Random.Shared.Next(IPV4AddressFragment)}.{Random.Shared.Next(IPV4AddressFragment)}.{Random.Shared.Next(IPV4AddressFragment)}";
+        }
+        return data;
+    }
+
+    /// <summary>
+    /// 生成随机 ipv6 地址
+    /// </summary>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    public static string[] GenerateRandomIPV6Addresses(uint count) {
+        var data = new string[count];
+        var sb = new StringBuilder(39);
+        for (int i = 0; i < count; i++) {
+            sb.Clear();
+            for (int j = 0; j < 8; j++) {
+                sb.Append($"{Convert.ToString(Random.Shared.Next(IPV6AddressFragment), 16).PadLeft(4, '0')}:");
+            }
+            sb.Remove(sb.Length - 1, 1);
+            data[i] = sb.ToString().ToUpperInvariant();
+        }
+        return data;
+    }
+
+    /// <summary>
+    /// 生成随机 MAC 地址
+    /// </summary>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    public static string[] GenerateRandomMACAddresses(uint count) {
+        var data = new string[count];
+        var sb = new StringBuilder(17);
+        for (int i = 0; i < count; i++) {
+            sb.Clear();
+            for (int j = 0; j < 6; j++) {
+                sb.Append($"{Convert.ToString(Random.Shared.Next(MACAddressFragment), 16).PadLeft(2, '0')}:");
+            }
+            sb.Remove(sb.Length - 1, 1);
+            data[i] = sb.ToString().ToUpperInvariant();
+        }
+        return data;
+    }
+
+    /// <summary>
     /// 获取压缩文件内容
     /// </summary>
     /// <param name="entryName">压缩文件名</param>
@@ -246,5 +352,19 @@ public class RandomGenerator {
     /// <param name="count">个数</param>
     /// <returns></returns>
     public static string[] GenerateRandomEnglishChineseNames(uint count) => GenerateRandomData(EnglishChineseNameEntryName, count);
+
+    /// <summary>
+    /// 生成随机英语单词
+    /// </summary>
+    /// <param name="count">个数</param>
+    /// <returns></returns>
+    public static string[] GenerateRandomEnglishWords(uint count) => GenerateRandomData(EnglishWordsEntryName, count);
+
+    /// <summary>
+    /// 生成随机中文词语
+    /// </summary>
+    /// <param name="count">个数</param>
+    /// <returns></returns>
+    public static string[] GenerateRandomChineseWords(uint count) => GenerateRandomData(ChineseWordsEntryName, count);
 
 }
