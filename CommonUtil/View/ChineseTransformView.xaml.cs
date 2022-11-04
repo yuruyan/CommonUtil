17cd14e7b70ba99ae56eeb465f62e774e7a33b5c
Luxiu123 <klt5466@163.com>
2022-11-04 16:28:17 +0800

    feat: 完成文件简繁体转换功能界面

--- CommonUtil/View/ChineseTransformView.xaml.cs
+++ CommonUtil/View/ChineseTransformView.xaml.cs
@@ -1,7 +1,14 @@
-﻿using CommonUtil.Core;
+﻿using CommonUITools.Utils;
+using CommonUtil.Core;
+using Microsoft.Win32;
 using NLog;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using MessageBox = CommonUITools.Widget.MessageBox;
 
 namespace CommonUtil.View;
 
@@ -10,6 +17,12 @@ public partial class ChineseTransformView : Page {
 
     public static readonly DependencyProperty InputTextProperty = DependencyProperty.Register("InputText", typeof(string), typeof(ChineseTransformView), new PropertyMetadata(""));
     public static readonly DependencyProperty OutputTextProperty = DependencyProperty.Register("OutputText", typeof(string), typeof(ChineseTransformView), new PropertyMetadata(""));
+    public static readonly DependencyProperty FileNameProperty = DependencyProperty.Register("FileName", typeof(string), typeof(ChineseTransformView), new PropertyMetadata(string.Empty));
+    public static readonly DependencyProperty HasFileProperty = DependencyProperty.Register("HasFile", typeof(bool), typeof(ChineseTransformView), new PropertyMetadata(false));
+    private readonly SaveFileDialog SaveFileDialog = new() {
+        Title = "保存文件",
+        Filter = "All Files|*.*"
+    };
 
     /// <summary>
     /// 输入文本
@@ -25,9 +38,91 @@ public partial class ChineseTransformView : Page {
         get { return (string)GetValue(OutputTextProperty); }
         set { SetValue(OutputTextProperty, value); }
     }
+    /// <summary>
+    /// 是否有文件
+    /// </summary>
+    public bool HasFile {
+        get { return (bool)GetValue(HasFileProperty); }
+        set { SetValue(HasFileProperty, value); }
+    }
+    /// <summary>
+    /// 文件名
+    /// </summary>
+    public string FileName {
+        get { return (string)GetValue(FileNameProperty); }
+        set { SetValue(FileNameProperty, value); }
+    }
 
     public ChineseTransformView() {
         InitializeComponent();
+    }
+
+    /// <summary>
+    /// 文字转简体
+    /// </summary>
+    private void StringToSimplified() {
+        OutputText = ChineseTransform.ToSimplified(InputText);
+    }
+
+    /// <summary>
+    /// 文件转繁体
+    /// </summary>
+    private void StringToTraditional() {
+        OutputText = ChineseTransform.ToTraditional(InputText);
+    }
+
+    /// <summary>
+    /// 文件转简体
+    /// </summary>
+    private async Task FileToSimplified() {
+        var inputPath = FileName;
+        if (SaveFileDialog.ShowDialog() != true) {
+            return;
+        }
+        var outputPath = SaveFileDialog.FileName;
+
+        await Task.Run(() => {
+            try {
+                ChineseTransform.FileToSimplified(inputPath, outputPath);
+                // 通知
+                CommonUITools.Widget.NotificationBox.Success(
+                    "转换成功",
+                    "点击打开",
+                    () => UIUtils.OpenFileInDirectoryAsync(outputPath)
+                );
+            } catch (IOException) {
+                MessageBox.Error("文件读取或写入失败");
+            } catch {
+                MessageBox.Error("转换失败");
+            }
+        });
+    }
+
+    /// <summary>
+    /// 文件转繁体
+    /// </summary>
+    private async Task FileToTraditional() {
+        var inputPath = FileName;
+        if (SaveFileDialog.ShowDialog() != true) {
+            return;
+        }
+        var outputPath = SaveFileDialog.FileName;
+
+        await Task.Run(() => {
+            try {
+                ChineseTransform.FileToTraditional(inputPath, outputPath);
+                // 通知
+                CommonUITools.Widget.NotificationBox.Success(
+                    "转换成功",
+                    "点击打开",
+                    () => UIUtils.OpenFileInDirectoryAsync(outputPath)
+                );
+            } catch (IOException) {
+                MessageBox.Error("文件读取或写入失败");
+            } catch {
+                MessageBox.Error("转换失败");
+            }
+        });
     }
 
     /// <summary>
@@ -37,7 +132,11 @@ public partial class ChineseTransformView : Page {
     /// <param name="e"></param>
     private void ToSimplifiedClick(object sender, RoutedEventArgs e) {
         e.Handled = true;
-        OutputText = ChineseTransform.ToSimplified(InputText);
+        if (!HasFile) {
+            StringToSimplified();
+            return;
+        }
+        ThrottleUtils.ThrottleAsync(ToSimplifiedClick, FileToSimplified);
     }
 
     /// <summary>
@@ -47,7 +146,11 @@ public partial class ChineseTransformView : Page {
     /// <param name="e"></param>
     private void ToTraditionalClick(object sender, RoutedEventArgs e) {
         e.Handled = true;
-        OutputText = ChineseTransform.ToTraditional(InputText);
+        if (!HasFile) {
+            StringToTraditional();
+            return;
+        }
+        ThrottleUtils.ThrottleAsync(ToTraditionalClick, FileToTraditional);
     }
 
     /// <summary>
@@ -58,7 +161,7 @@ public partial class ChineseTransformView : Page {
     private void CopyResultClick(object sender, RoutedEventArgs e) {
         e.Handled = true;
         Clipboard.SetDataObject(OutputText);
-        CommonUITools.Widget.MessageBox.Success("已复制");
+        MessageBox.Success("已复制");
     }
 
     /// <summary>
@@ -68,8 +171,26 @@ public partial class ChineseTransformView : Page {
     /// <param name="e"></param>
     private void ClearInputClick(object sender, RoutedEventArgs e) {
         e.Handled = true;
-        InputText = string.Empty;
-        OutputText = string.Empty;
+        FileName = InputText = OutputText = string.Empty;
+        DragDropTextBox.Clear();
+    }
+
+    /// <summary>
+    /// DragDropEvent
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void DragDropEventHandler(object sender, object e) {
+        if (e is IEnumerable<string> array) {
+            if (!array.Any()) {
+                return;
+            }
+            // 判断是否为文件
+            if (File.Exists(array.First())) {
+                FileName = array.First();
+            } else {
+                DragDropTextBox.Clear();
+            }
+        }
     }
 }
-
