446031762689010c0ac315b09516eded3519f048
Luxiu123 <klt5466@163.com>
2022-11-04 23:22:58 +0800

    refactor: 重构 CommonRegexListDialog

--- CommonUtil/Resource/Resource.Designer.cs
+++ CommonUtil/Resource/Resource.Designer.cs
@@ -69,5 +69,15 @@ namespace CommonUtil.Resource {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] CommonRegex {
+            get {
+                object obj = ResourceManager.GetObject("CommonRegex", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
     }
 }
--- CommonUtil/View/Dialog/CommonRegexListDialog.xaml.cs
+++ CommonUtil/View/Dialog/CommonRegexListDialog.xaml.cs
@@ -1,9 +1,8 @@
 ﻿using ModernWpf.Controls;
 using Newtonsoft.Json;
 using NLog;
-using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Text;
 using System.Windows;
 
 namespace CommonUtil.View;
@@ -11,37 +10,32 @@ namespace CommonUtil.View;
 public partial class CommonRegexListDialog : ContentDialog {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-    private static readonly List<KeyValuePair<string, string>> CommonRegexList = new();
-    private static readonly string CommonRegexPath = "resource/CommonRegex.json";
+    private static readonly IEnumerable<KeyValuePair<string, string>> CommonRegexList;
 
     /// <summary>
     /// 加载 CommonRegexList
     /// </summary>
     static CommonRegexListDialog() {
-        try {
-            var list = JsonConvert.DeserializeObject<List<KeyValuePair<string, string>>>(File.ReadAllText(CommonRegexPath));
-            if (list != null) {
-                CommonRegexList = list;
-            }
-        } catch (FileNotFoundException) {
-            Logger.Error("cannot find the file " + CommonRegexPath);
-        } catch (Exception e) {
-            Logger.Error(e);
+        var list = JsonConvert.DeserializeObject<IEnumerable<KeyValuePair<string, string>>>(
+            Encoding.UTF8.GetString(Resource.Resource.CommonRegex)
+        );
+        if (list == null) {
+            throw new JsonSerializationException("解析 CommonRegexList 失败");
         }
+        CommonRegexList = list;
     }
 
     /// <summary>
     /// 正则列表
     /// </summary>
-    public List<KeyValuePair<string, string>> RegexList {
-        get { return (List<KeyValuePair<string, string>>)GetValue(RegexListProperty); }
+    public IEnumerable<KeyValuePair<string, string>> RegexList {
+        get { return (IEnumerable<KeyValuePair<string, string>>)GetValue(RegexListProperty); }
         set { SetValue(RegexListProperty, value); }
     }
-    public static readonly DependencyProperty RegexListProperty = DependencyProperty.Register("RegexList", typeof(List<KeyValuePair<string, string>>), typeof(CommonRegexListDialog), new PropertyMetadata());
+    public static readonly DependencyProperty RegexListProperty = DependencyProperty.Register("RegexList", typeof(IEnumerable<KeyValuePair<string, string>>), typeof(CommonRegexListDialog), new PropertyMetadata());
 
     public CommonRegexListDialog() {
         RegexList = CommonRegexList;
         InitializeComponent();
     }
 }
-
