c7b57f307b4b29ad06e5724b2c0f3468966abc97
Luxiu123 <klt5466@163.com>
2022-11-04 23:02:15 +0800

    feat: 添加 AsciiTableView 多项复制功能

--- CommonUtil/View/AsciiTableView.xaml.cs
+++ CommonUtil/View/AsciiTableView.xaml.cs
@@ -2,6 +2,7 @@
 using CommonUtil.Model;
 using NLog;
 using System.Collections.ObjectModel;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -41,12 +42,18 @@ public partial class AsciiTableView : Page {
     /// <param name="e"></param>
     private void CopyDetailClickHandler(object sender, RoutedEventArgs e) {
         e.Handled = true;
-        if (sender is FrameworkElement element) {
-            if (element.DataContext is AsciiInfo info) {
-                Clipboard.SetDataObject($"{info.Binary}\t{info.Octal}\t{info.Decimal}\t{info.HexaDecimal}\t{info.Character}\t{info.HtmlEntity}\t{info.Description}");
-                CommonUITools.Widget.MessageBox.Success("已复制");
-            }
+        var sb = new StringBuilder();
+        foreach (var item in AsciiListView.SelectedItems) {
+            var info = (AsciiInfo)item;
+            sb.Append(info.Binary).Append('\t');
+            sb.Append(info.Octal).Append('\t');
+            sb.Append(info.Decimal).Append('\t');
+            sb.Append(info.HexaDecimal).Append('\t');
+            sb.Append(info.Character).Append('\t');
+            sb.Append(info.HtmlEntity).Append('\t');
+            sb.Append(info.Description).Append('\n');
         }
+        Clipboard.SetDataObject(sb.ToString());
+        CommonUITools.Widget.MessageBox.Success("已复制");
     }
 }
-
