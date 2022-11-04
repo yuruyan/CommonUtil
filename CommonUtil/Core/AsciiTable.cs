1d4ced0756a4c0bf75f7b997d03b82b30784b303
Luxiu123 <klt5466@163.com>
2022-11-04 23:11:05 +0800

    refactor: 重构 AsciiTable

--- CommonUtil/Core/AsciiTable.cs
+++ CommonUtil/Core/AsciiTable.cs
@@ -3,54 +3,57 @@ using Newtonsoft.Json;
 using NLog;
 using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Text;
 
 namespace CommonUtil.Core;
 
 public class AsciiTable {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
+    /// <summary>
+    /// 全部字符
+    /// </summary>
     private static readonly List<AsciiInfo> AsciiInfoList = new();
-    private static readonly List<AsciiInfo> AsciiInfoControlList = new(); // 控制字符
-    private static readonly List<AsciiInfo> AsciiInfoNormalList = new(); // 非控制字符
-    private static readonly List<AsciiInfo> AsciiInfoExtendedList = new(); // 扩展字符
-
-    private static readonly string ResourcePath = "./resource/ascii.json";
+    /// <summary>
+    /// 控制字符
+    /// </summary>
+    private static readonly List<AsciiInfo> AsciiInfoControlList = new();
+    /// <summary>
+    /// 非控制字符
+    /// </summary>
+    private static readonly List<AsciiInfo> AsciiInfoNormalList = new();
+    /// <summary>
+    /// 扩展字符
+    /// </summary>
+    private static readonly List<AsciiInfo> AsciiInfoExtendedList = new();
 
     /// <summary>
     /// 加载数据
     /// </summary>
     static AsciiTable() {
-        if (!File.Exists(ResourcePath)) {
-            CommonUITools.Widget.MessageBox.Error("找不到文件 " + ResourcePath);
-            Logger.Error("cannot find the file " + ResourcePath);
-            return;
+        var list = JsonConvert.DeserializeObject<List<AsciiInfo>>(
+            Encoding.UTF8.GetString(Resource.Resource.Ascii)
+        );
+        if (list == null) {
+            throw new Exception("加载 AsciiTable 失败");
         }
-        try {
-            List<AsciiInfo>? list = JsonConvert.DeserializeObject<List<AsciiInfo>>(File.ReadAllText(ResourcePath));
-            if (list == null) {
-                throw new Exception("List<AsciiInfo>? list is null");
-            }
-            AsciiInfoList.AddRange(list);
-            #region 控制字符
-            for (int i = 0; i < 32; i++) {
-                AsciiInfoControlList.Add(AsciiInfoList[i]);
-            }
-            AsciiInfoControlList.Add(AsciiInfoList[127]);
-            #endregion
-            #region 非控制字符
-            for (int i = 32; i < 127; i++) {
-                AsciiInfoNormalList.Add(AsciiInfoList[i]);
-            }
-            #endregion
-            #region 其他字符
-            for (int i = 128; i < 256; i++) {
-                AsciiInfoNormalList.Add(AsciiInfoList[i]);
-            }
-            #endregion
-        } catch (Exception e) {
-            Logger.Error(e);
+        AsciiInfoList.AddRange(list);
+        #region 控制字符
+        for (int i = 0; i < 32; i++) {
+            AsciiInfoControlList.Add(AsciiInfoList[i]);
         }
+        AsciiInfoControlList.Add(AsciiInfoList[127]);
+        #endregion
+        #region 非控制字符
+        for (int i = 32; i < 127; i++) {
+            AsciiInfoNormalList.Add(AsciiInfoList[i]);
+        }
+        #endregion
+        #region 其他字符
+        for (int i = 128; i < 256; i++) {
+            AsciiInfoNormalList.Add(AsciiInfoList[i]);
+        }
+        #endregion
     }
 
     /// <summary>
