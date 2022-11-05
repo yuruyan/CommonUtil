0841e7990c4842ac8e0fa1b502ef26d60b77c504
Luxiu123 <klt5466@163.com>
2022-11-05 19:42:40 +0800

    feat: 添加 TextTool 文件支持功能

--- CommonUtil/Core/TextTool.cs
+++ CommonUtil/Core/TextTool.cs
@@ -1,6 +1,7 @@
 ﻿using CommonUITools.Utils;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -48,9 +49,43 @@ public class TextTool {
             return string.Join(mergeSymbol, text.ToHashSet());
         }
         if (!trim) {
-            return string.Join(mergeSymbol, new HashSet<string>(CommonUtils.NormalizeMultipleLineText(text).Split(splitSymbol)));
+            return string.Join(
+                mergeSymbol,
+                new HashSet<string>(CommonUtils.NormalizeMultipleLineText(text).Split(splitSymbol))
+            );
         }
-        return string.Join(mergeSymbol, new HashSet<string>(CommonUtils.NormalizeMultipleLineText(text).Split(splitSymbol, StringSplitOptions.TrimEntries)));
+        return string.Join(
+            mergeSymbol,
+            new HashSet<string>(
+                CommonUtils.NormalizeMultipleLineText(text).Split(splitSymbol, StringSplitOptions.TrimEntries)
+            )
+        );
+    }
+
+    /// <summary>
+    /// 文件文本去重
+    /// </summary>
+    /// <param name="inputPath"></param>
+    /// <param name="outputPath"></param>
+    /// <param name="splitSymbol">分隔符</param>
+    /// <param name="mergeSymbol">合并符</param>
+    /// <param name="trim">移除元素首尾空白</param>
+    public static void FileRemoveDuplicate(
+        string inputPath,
+        string outputPath,
+        string splitSymbol,
+        string mergeSymbol,
+        bool trim = false
+    ) {
+        File.WriteAllText(
+            outputPath,
+            RemoveDuplicate(
+                File.ReadAllText(inputPath),
+                splitSymbol,
+                mergeSymbol,
+                trim
+            )
+        );
     }
 
     /// <summary>
@@ -63,12 +98,35 @@ public class TextTool {
     }
 
     /// <summary>
+    /// 文件文本去除首尾空格
+    /// </summary>
+    /// <param name="inputPath"></param>
+    /// <param name="outputPath"></param>
+    public static void FileTrimText(string inputPath, string outputPath) {
+        File.WriteAllText(outputPath, TrimText(File.ReadAllText(inputPath)));
+    }
+
+    /// <summary>
     /// 去除空白行
     /// </summary>
     /// <param name="text"></param>
     /// <returns></returns>
     public static string RemoveWhiteSpaceLine(string text) {
-        return string.Join('\n', CommonUtils.NormalizeMultipleLineText(text).Split('\n').Where(s => s.Trim().Any()));
+        return string.Join(
+            '\n',
+            CommonUtils.NormalizeMultipleLineText(text)
+                .Split('\n')
+                .Where(s => s.Trim().Any())
+        );
+    }
+
+    /// <summary>
+    /// 文件文本去除空白行
+    /// </summary>
+    /// <param name="inputPath"></param>
+    /// <param name="outputPath"></param>
+    public static void FileRemoveWhiteSpaceLine(string inputPath, string outputPath) {
+        File.WriteAllText(outputPath, RemoveWhiteSpaceLine(File.ReadAllText(inputPath)));
     }
 
     /// <summary>
@@ -77,7 +135,21 @@ public class TextTool {
     /// <param name="text"></param>
     /// <returns></returns>
     public static string TrimLine(string text) {
-        return string.Join('\n', CommonUtils.NormalizeMultipleLineText(text).Split('\n').Select(s => s.Trim()));
+        return string.Join(
+            '\n',
+            CommonUtils.NormalizeMultipleLineText(text)
+                .Split('\n')
+                .Select(s => s.Trim())
+        );
+    }
+
+    /// <summary>
+    /// 文件文本去除每行首尾空格
+    /// </summary>
+    /// <param name="inputPath"></param>
+    /// <param name="outputPath"></param>
+    public static void FileTrimLine(string inputPath, string outputPath) {
+        File.WriteAllText(outputPath, TrimLine(File.ReadAllText(inputPath)));
     }
 
     /// <summary>
@@ -87,6 +159,16 @@ public class TextTool {
     /// <returns></returns>
     public static string ReplaceMultipleWhiteSpaceWithOne(string text) {
         return MultipleWhiteSpace.Replace(text, " ");
+    }
+
+    /// <summary>
+    /// 文件文本将多个空白字符替换成一个空格
+    /// </summary>
+    /// <param name="inputPath"></param>
+    /// <param name="outputPath"></param>
+    /// <returns></returns>
+    public static void FileReplaceMultipleWhiteSpaceWithOne(string inputPath, string outputPath) {
+        File.WriteAllText(outputPath, ReplaceMultipleWhiteSpaceWithOne(File.ReadAllText(inputPath)));
     }
 
     /// <summary>
@@ -105,6 +187,15 @@ public class TextTool {
     }
 
     /// <summary>
+    /// 文件文本半角转全角
+    /// </summary>
+    /// <param name="inputPath"></param>
+    /// <param name="outputPath"></param>
+    public static void FileHalfCharToFullChar(string inputPath, string outputPath) {
+        File.WriteAllText(outputPath, HalfCharToFullChar(File.ReadAllText(inputPath)));
+    }
+
+    /// <summary>
     /// 全角转半角
     /// </summary>
     /// <param name="fullCharText"></param>
@@ -117,6 +208,15 @@ public class TextTool {
             }
         }
         return new(array);
+    }
+
+    /// <summary>
+    /// 文件文本全角转半角
+    /// </summary>
+    /// <param name="inputPath"></param>
+    /// <param name="outputPath"></param>
+    public static void FileFullCharToHalfChar(string inputPath, string outputPath) {
+        File.WriteAllText(outputPath, FullCharToHalfChar(File.ReadAllText(inputPath)));
     }
 
     /// <summary>
@@ -134,6 +234,17 @@ public class TextTool {
     }
 
     /// <summary>
+    /// 文件文本添加行号
+    /// </summary>
+    /// <param name="inputPath"></param>
+    /// <param name="outputPath"></param>
+    /// <param name="extraText">额外文本</param>
+    /// <returns></returns>
+    public static void FilePrependLineNumber(string inputPath, string outputPath, string extraText) {
+        File.WriteAllText(outputPath, PrependLineNumber(File.ReadAllText(inputPath), extraText));
+    }
+
+    /// <summary>
     /// 英文单词正则
     /// </summary>
     public static readonly Regex EnglishWordRegex = new(@"[a-z]+", RegexOptions.IgnoreCase);
@@ -147,11 +258,23 @@ public class TextTool {
     /// 英文两边加空格
     /// </summary>
     /// <param name="text"></param>
+    /// <param name="includeNumber">包括数字</param>
     /// <returns></returns>
     public static string AddEnglishWordBraces(string text, bool includeNumber = false) {
         if (includeNumber) {
             return EnglishWordNumberRegex.Replace(text, match => $" {match} ");
         }
         return EnglishWordRegex.Replace(text, match => $" {match} ");
+    }
+
+    /// <summary>
+    /// 文件文本英文两边加空格
+    /// </summary>
+    /// <param name="inputPath"></param>
+    /// <param name="outputPath"></param>
+    /// <param name="includeNumber">包括数字</param>
+    /// <returns></returns>
+    public static void FileAddEnglishWordBraces(string inputPath, string outputPath, bool includeNumber = false) {
+        File.WriteAllText(outputPath, AddEnglishWordBraces(File.ReadAllText(inputPath), includeNumber));
     }
 }
