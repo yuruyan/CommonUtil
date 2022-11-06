19055bf02ccdb2c9ca1800824d65011a2e145a34
Luxiu123 <klt5466@163.com>
2022-11-06 10:02:54 +0800

    feat: 添加 WhiteSpaceProcessView 文件支持功能

--- CommonUtil/View/TextTool/WhiteSpaceProcessView.xaml.cs
+++ CommonUtil/View/TextTool/WhiteSpaceProcessView.xaml.cs
@@ -1,8 +1,15 @@
-﻿using CommonUtil.Core;
+﻿using CommonUITools.Utils;
+using CommonUITools.View;
+using CommonUtil.Core;
+using Microsoft.Win32;
 using NLog;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using MessageBox = CommonUITools.Widget.MessageBox;
 
 namespace CommonUtil.View;
 
@@ -11,6 +18,12 @@ public partial class WhiteSpaceProcessView : Page {
 
     public static readonly DependencyProperty OutputTextProperty = DependencyProperty.Register("OutputText", typeof(string), typeof(WhiteSpaceProcessView), new PropertyMetadata(""));
     public static readonly DependencyProperty InputTextProperty = DependencyProperty.Register("InputText", typeof(string), typeof(WhiteSpaceProcessView), new PropertyMetadata(""));
+    public static readonly DependencyProperty FileNameProperty = DependencyProperty.Register("FileName", typeof(string), typeof(WhiteSpaceProcessView), new PropertyMetadata(string.Empty));
+    public static readonly DependencyProperty HasFileProperty = DependencyProperty.Register("HasFile", typeof(bool), typeof(WhiteSpaceProcessView), new PropertyMetadata(false));
+    private readonly SaveFileDialog SaveFileDialog = new() {
+        Title = "保存文件",
+        Filter = "All Files|*.*"
+    };
 
     /// <summary>
     /// 输入文本
@@ -26,6 +39,20 @@ public partial class WhiteSpaceProcessView : Page {
         get { return (string)GetValue(OutputTextProperty); }
         set { SetValue(OutputTextProperty, value); }
     }
+    /// <summary>
+    /// 是否有文件
+    /// </summary>
+    public bool HasFile {
+        get { return (bool)GetValue(HasFileProperty); }
+        set { SetValue(HasFileProperty, value); }
+    }
+    /// <summary>
+    /// 文件名
+    /// </summary>
+    public string FileName {
+        get { return (string)GetValue(FileNameProperty); }
+        set { SetValue(FileNameProperty, value); }
+    }
 
     public WhiteSpaceProcessView() {
         InitializeComponent();
@@ -36,34 +63,92 @@ public partial class WhiteSpaceProcessView : Page {
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
-    private void TextProcessClick(object sender, RoutedEventArgs e) {
+    private async void TextProcessClick(object sender, RoutedEventArgs e) {
         e.Handled = true;
+        bool trimText = TrimTextCheckBox.IsChecked == true;
+        bool removeWhiteSpace = RemoveWhiteSpaceLineCheckBox.IsChecked == true;
+        bool trimLine = TrimLineCheckBox.IsChecked == true;
+        bool replaceMultiWhiteSpace = ReplaceMultipleWhiteSpaceWithOneCheckBox.IsChecked == true;
+
+        // 二进制文件警告
+        if (HasFile && CommonUtils.IsLikelyBinaryFile(FileName)) {
+            WarningDialog dialog = WarningDialog.Shared;
+            dialog.DetailText = "文件可能是二进制文件，是否继续？";
+            if (await dialog.ShowAsync() != ModernWpf.Controls.ContentDialogResult.Primary) {
+                return;
+            }
+        }
+        // 输入检查
+        if (!HasFile && InputText.Length == 0) {
+            MessageBox.Info("请输入文本");
+            return;
+        }
+
+        // 文本处理
+        if (!HasFile) {
+            TextProcessString(trimText, removeWhiteSpace, trimLine, replaceMultiWhiteSpace);
+            return;
+        }
+        ThrottleUtils.ThrottleAsync(
+            TextProcessClick,
+            () => TextProcessFile(trimText, removeWhiteSpace, trimLine, replaceMultiWhiteSpace)
+        );
+    }
+
+    /// <summary>
+    /// 文本处理
+    /// </summary>
+    /// <param name="trimText"></param>
+    /// <param name="removeWhiteSpace"></param>
+    /// <param name="trimLine"></param>
+    /// <param name="replaceMultiWhiteSpace"></param>
+    private void TextProcessString(bool trimText, bool removeWhiteSpace, bool trimLine, bool replaceMultiWhiteSpace) {
         var s = InputText;
-        if (TrimTextCheckBox.IsChecked == true) {
+        if (trimText) {
             s = TextTool.TrimText(s);
         }
-        if (RemoveWhiteSpaceLineCheckBox.IsChecked == true) {
+        if (removeWhiteSpace) {
             s = TextTool.RemoveWhiteSpaceLine(s);
         }
-        if (TrimLineCheckBox.IsChecked == true) {
+        if (trimLine) {
             s = TextTool.TrimLine(s);
         }
-        if (ReplaceMultipleWhiteSpaceWithOneCheckBox.IsChecked == true) {
+        if (replaceMultiWhiteSpace) {
             s = TextTool.ReplaceMultipleWhiteSpaceWithOne(s);
         }
         OutputText = s;
     }
 
-    /// <summary>
-    /// 检查输入合法性
-    /// </summary>
-    /// <returns></returns>
-    private bool CheckInputValidation() {
-        bool r = InputText.Trim().Any();
-        if (!r) {
-            CommonUITools.Widget.MessageBox.Info("请输入文本");
+    private async Task TextProcessFile(bool trimText, bool removeWhiteSpace, bool trimLine, bool replaceMultiWhiteSpace) {
+        var text = InputText;
+        var inputPath = FileName;
+        if (SaveFileDialog.ShowDialog() != true) {
+            return;
         }
-        return r;
+        var outputPath = SaveFileDialog.FileName;
+
+        await Task.Run(() => {
+            try {
+                if (trimText) {
+                    TextTool.FileTrimText(inputPath, outputPath);
+                }
+                if (removeWhiteSpace) {
+                    TextTool.FileRemoveWhiteSpaceLine(inputPath, outputPath);
+                }
+                if (trimLine) {
+                    TextTool.FileTrimLine(inputPath, outputPath);
+                }
+                if (replaceMultiWhiteSpace) {
+                    TextTool.FileReplaceMultipleWhiteSpaceWithOne(inputPath, outputPath);
+                }
+                // 通知
+                UIUtils.NotificationOpenFileInDirectoryAsync(outputPath);
+            } catch (IOException) {
+                MessageBox.Error("文件读取或写入失败");
+            } catch {
+                MessageBox.Error("失败");
+            }
+        });
     }
 
     /// <summary>
@@ -74,7 +159,7 @@ public partial class WhiteSpaceProcessView : Page {
     private void CopyResultClick(object sender, RoutedEventArgs e) {
         e.Handled = true;
         Clipboard.SetDataObject(OutputText);
-        CommonUITools.Widget.MessageBox.Success("已复制");
+        MessageBox.Success("已复制");
     }
 
     /// <summary>
@@ -84,9 +169,27 @@ public partial class WhiteSpaceProcessView : Page {
     /// <param name="e"></param>
     private void ClearInputClick(object sender, RoutedEventArgs e) {
         e.Handled = true;
-        InputText = string.Empty;
-        OutputText = string.Empty;
+        InputText = OutputText = string.Empty;
+        DragDropTextBox.Clear();
+    }
+
+    /// <summary>
+    /// DragDropEvent
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void DragDropEventHandler(object sender, object e) {
+        if (e is IEnumerable<string> array) {
+            if (!array.Any()) {
+                return;
+            }
+            // 判断是否为文件
+            if (File.Exists(array.First())) {
+                FileName = array.First();
+            } else {
+                DragDropTextBox.Clear();
+            }
+        }
     }
 
 }
-
