5f108b9f7493d4b3f8d29e19e77852adba194e9b
Luxiu123 <klt5466@163.com>
2022-11-15 19:52:27 +0800

    feat: 添加 翻转文本 功能

--- CommonUtil/Core/TextTool.cs
+++ CommonUtil/Core/TextTool.cs
@@ -408,4 +408,34 @@ public partial class TextTool {
     public static void FileToSentenceCase(string inputPath, string outputPath) {
         File.WriteAllText(outputPath, ToSentenceCase(File.ReadAllText(inputPath)));
     }
+
+    public enum InversionMode {
+        Horizontal,
+        Vertical,
+        Both
+    }
+
+    /// <summary>
+    /// 翻转文本
+    /// </summary>
+    /// <param name="text"></param>
+    /// <param name="mode"></param>
+    /// <returns></returns>
+    public static string InvertText(string text, InversionMode mode)
+        => mode switch {
+            InversionMode.Horizontal => string.Join('\n', text.Split('\n').Select(s => string.Join("", s.Reverse()))),
+            InversionMode.Vertical => string.Join('\n', text.Split('\n').Reverse()),
+            InversionMode.Both => string.Join("", text.Reverse()),
+            _ => InvertText(text, InversionMode.Both),
+        };
+
+    /// <summary>
+    /// 文件文本翻转
+    /// </summary>
+    /// <param name="inputPath"></param>
+    /// <param name="outputPath"></param>
+    /// <param name="mode"></param>
+    public static void FileInvertText(string inputPath, string outputPath, InversionMode mode) {
+        File.WriteAllText(outputPath, InvertText(File.ReadAllText(inputPath), mode));
+    }
 }
