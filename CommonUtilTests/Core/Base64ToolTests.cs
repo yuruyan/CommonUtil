8bbfe9d235f5305dfd071cb1eaee61967ba4a00a
Luxiu123 <klt5466@163.com>
2022-11-21 17:07:09 +0800

    mod: 创建 CommonUtilTests 项目

---
+++ CommonUtilTests/Core/Base64ToolTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommonUtil.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonUtil.Core.Tests;
+
+[TestClass()]
+public class Base64ToolTests {
+    [TestMethod()]
+    public void Base64DecodeString() {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod()]
+    public void Base64DecodeString1() {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod()]
+    public void Base64EncodeString() {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod()]
+    public void Base64EncodeString1() {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod()]
+    public void Base64EncodeFile() {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod()]
+    public void Base64EncodeFile1() {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod()]
+    public void Base64DecodeFile() {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod()]
+    public void Base64DecodeFile1() {
+        throw new NotImplementedException();
+    }
+
+    [TestMethod()]
+    public void TryDecode() {
+        throw new NotImplementedException();
+    }
+}
