e38f7463fbd39342d0a0f3fa6b004d99f54ef89d
Luxiu123 <klt5466@163.com>
2022-11-21 18:02:13 +0800

    mod: 完善 Base64ToolTests 测试

--- CommonUtilTests/Core/Base64ToolTests.cs
+++ CommonUtilTests/Core/Base64ToolTests.cs
@@ -5,53 +5,100 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommonUtilTests.Utils;
 
 namespace CommonUtil.Core.Tests;
 
 [TestClass()]
 public class Base64ToolTests {
+    private const string SourceFile = "resource/ok.png";
+    private const string OutputFile1 = "temp/UFM2VJHID55OHRZ6.png";
+    private const string OutputFile2 = "temp/QIHJD1OKGHZY8U26.png";
+
     [TestMethod()]
-    public void Base64DecodeString() {
-        throw new NotImplementedException();
+    public void Base64DecodeString_string_Encoding() {
+        Assert.AreEqual(
+            Base64Tool.Base64DecodeString("aGVsbG8gd29ybGQ=", Encoding.UTF8),
+            "hello world"
+        );
+        Assert.AreEqual(
+            Base64Tool.Base64DecodeString("aGVsbG8gd29ybGQ=", Encoding.ASCII),
+            "hello world"
+        );
+        Assert.AreEqual(
+            Base64Tool.Base64DecodeString("5L2g5aW977yMSGVsbG8=", Encoding.UTF8),
+            "你好，Hello"
+        );
     }
 
     [TestMethod()]
-    public void Base64DecodeString1() {
-        throw new NotImplementedException();
+    public void Base64DecodeString_string() {
+        Assert.AreEqual(
+            Base64Tool.Base64DecodeString("5L2g5aW977yMSGVsbG8="),
+            "你好，Hello"
+        );
+        Assert.AreEqual(
+            Base64Tool.Base64DecodeString("QmFzZTY05Yqg5a+G5ZKMQmFzZTY06Kej5a+G"),
+            "Base64加密和Base64解密"
+        );
     }
 
     [TestMethod()]
-    public void Base64EncodeString() {
-        throw new NotImplementedException();
+    public void Base64EncodeString_string_Encoding() {
+        Assert.AreEqual(
+            Base64Tool.Base64EncodeString("Base64 在线编码解码", Encoding.UTF8),
+            "QmFzZTY0IOWcqOe6v+e8lueggeino+eggQ=="
+        );
+        Assert.AreEqual(
+            Base64Tool.Base64EncodeString("QmFzZTY0IOWcqOe6v+e8lueggeino+eggQ==", Encoding.ASCII),
+            "UW1GelpUWTBJT1djcU9lNnYrZThsdWVnZ2Vpbm8rZWdnUT09"
+        );
     }
 
     [TestMethod()]
-    public void Base64EncodeString1() {
-        throw new NotImplementedException();
+    public void Base64EncodeString_string() {
+        Assert.AreEqual(
+            Base64Tool.Base64EncodeString("请输入要进行 Base64 编码或解码的字符"),
+            "6K+36L6T5YWl6KaB6L+b6KGMIEJhc2U2NCDnvJbnoIHmiJbop6PnoIHnmoTlrZfnrKY="
+        );
+        Assert.AreEqual(
+            Base64Tool.Base64EncodeString("编码完毕，原文本字节数：50，编码后字节数：68"),
+            "57yW56CB5a6M5q+V77yM5Y6f5paH5pys5a2X6IqC5pWw77yaNTDvvIznvJbnoIHlkI7lrZfoioLmlbDvvJo2OA=="
+        );
     }
 
     [TestMethod()]
-    public void Base64EncodeFile() {
-        throw new NotImplementedException();
+    public void Base64EncodeFile_string() {
+        File.WriteAllText(OutputFile1, Base64Tool.Base64EncodeFile(SourceFile));
+        File.WriteAllBytes(OutputFile1, Base64Tool.Base64DecodeFile(OutputFile1));
+        Assert.IsTrue(TestUtils.AreFilesEqual(SourceFile, OutputFile1));
     }
 
     [TestMethod()]
-    public void Base64EncodeFile1() {
-        throw new NotImplementedException();
+    public void Base64EncodeFile_string_string_CancellationToken() {
+        Assert.IsTrue(Base64Tool.Base64EncodeFile(SourceFile, OutputFile1) > 0);
+        Assert.IsTrue(Base64Tool.Base64DecodeFile(OutputFile1, OutputFile2) > 0);
+        Assert.IsTrue(TestUtils.AreFilesEqual(SourceFile, OutputFile2));
     }
 
     [TestMethod()]
-    public void Base64DecodeFile() {
-        throw new NotImplementedException();
+    public void Base64DecodeFile_string() {
+        File.WriteAllText(OutputFile1, Base64Tool.Base64EncodeFile(SourceFile));
+        File.WriteAllBytes(OutputFile1, Base64Tool.Base64DecodeFile(OutputFile1));
+        Assert.IsTrue(TestUtils.AreFilesEqual(SourceFile, OutputFile1));
     }
 
     [TestMethod()]
-    public void Base64DecodeFile1() {
-        throw new NotImplementedException();
+    public void Base64DecodeFile_string_string_CancellationToken() {
+        Assert.IsTrue(Base64Tool.Base64EncodeFile(SourceFile, OutputFile1) > 0);
+        Assert.IsTrue(Base64Tool.Base64DecodeFile(OutputFile1, OutputFile2) > 0);
+        Assert.IsTrue(TestUtils.AreFilesEqual(SourceFile, OutputFile2));
     }
 
     [TestMethod()]
     public void TryDecode() {
-        throw new NotImplementedException();
+        Assert.IsNotNull(Base64Tool.TryDecode("VGltZVN0YW1w"));
+        Assert.IsNotNull(Base64Tool.TryDecode("5ZCM5LiW55WM55u45LqS5Lqk6J6N55u45LqS5oiQ5bCx"));
+        Assert.IsNotNull(Base64Tool.TryDecode("5Zyo57q/YmFzZTY06Kej56CBL+e8lueggeW3peWFt+aYr+S4gOS4quWPr+S7peWwhuWtl+espuS4sui/m+ihjGJhc2U2NOino+eggS/nvJbnoIHnmoTlt6Xlhbc="));
     }
 }
---
+++ CommonUtilTests/Utils/TestUtils.cs
@@ -0,0 +1,16 @@
+﻿using CommonUtil.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonUtilTests.Utils;
+
+internal static class TestUtils {
+    public static bool AreFilesEqual(string file1, string file2) {
+        using var stream1 = File.OpenRead(file1);
+        using var stream2 = File.OpenRead(file2);
+        return DataDigest.MD5Digest(stream1) == DataDigest.MD5Digest(stream2);
+    }
+}
