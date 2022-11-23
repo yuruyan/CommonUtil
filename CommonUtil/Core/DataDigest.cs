fc2da07acabd658742c59c6de44bb5dbbede3f3a
Luxiu123 <klt5466@163.com>
2022-11-23 23:28:22 +0800

    refactor: 重构 DataDigest

--- CommonUtil/Core/DataDigest.cs
+++ CommonUtil/Core/DataDigest.cs
@@ -2,14 +2,18 @@
 using Org.BouncyCastle.Crypto.Digests;
 using Org.BouncyCastle.Utilities.Encoders;
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace CommonUtil.Core;
 
-public class DataDigest {
+public static class DataDigest {
+    /// <summary>
+    /// 默认读取缓冲区大小
+    /// </summary>
+    private const int FileReadBuffer = 8192;
+
     /// <summary>
     /// 摘要算法
     /// </summary>
@@ -25,64 +29,36 @@ public class DataDigest {
     }
 
     /// <summary>
-    /// 读取缓冲区队列
-    /// </summary>
-    private static readonly ConcurrentQueue<byte[]> ReadBufferQueue = new();
-    /// <summary>
-    /// 默认读取缓冲区个数
-    /// </summary>
-    private const int DefaultReadBufferSize = 8;
-    /// <summary>
-    /// 默认读取缓冲区大小
-    /// </summary>
-    private const int FileReadBuffer = 4096 << 2;
-    private static readonly HashSet<FileStream> StoppingDigestSet = new(DefaultReadBufferSize);
-
-    static DataDigest() {
-        // 初始化队列
-        for (int i = 0; i < DefaultReadBufferSize; i++) {
-            ReadBufferQueue.Enqueue(new byte[FileReadBuffer]);
-        }
-    }
-
-    /// <summary>
     /// 摘要算法
     /// </summary>
     /// <param name="stream"></param>
     /// <param name="digest"></param>
-    /// <param name="callback">回调，参数为总读取的大小</param>
+    /// <param name="cancellationToken"></param>
+    /// <param name="callback">进度回调，参数为进度百分比</param>
     /// <returns></returns>
-    private static string GeneralDigest(FileStream stream, IDigest digest, Action<long>? callback = null) {
-        // 从队列中获取缓存
-        if (!ReadBufferQueue.TryDequeue(out var buffer)) {
-            buffer = new byte[FileReadBuffer];
-        }
-        byte[] resultBuffer = new byte[digest.GetDigestSize()];
-        int read;
-        long totalRead = 0;
-        while ((read = stream.Read(buffer, 0, FileReadBuffer)) > 0) {
+    private static string? GeneralDigest(
+        FileStream stream,
+        IDigest digest,
+        CancellationToken? cancellationToken = null,
+        Action<double>? callback = null
+    ) {
+        var buffer = new byte[FileReadBuffer];
+        var resultBuffer = new byte[digest.GetDigestSize()];
+        int readCound;
+        long totalRead = 0, streamLength = stream.Length;
+        while ((readCound = stream.Read(buffer, 0, buffer.Length)) > 0) {
             // 终止计算
-            if (StoppingDigestSet.Count > 0 && StoppingDigestSet.Contains(stream)) {
-                StoppingDigestSet.Remove(stream);
-                // 添加到队列中
-                ReadBufferQueue.Enqueue(buffer);
-                return string.Empty;
+            if (cancellationToken?.IsCancellationRequested == true) {
+                return null;
             }
-            digest.BlockUpdate(buffer, 0, read);
-            totalRead += read;
-            callback?.Invoke(totalRead);
+            digest.BlockUpdate(buffer, 0, readCound);
+            totalRead += readCound;
+            callback?.Invoke((double)totalRead / streamLength);
         }
         digest.DoFinal(resultBuffer, 0);
-        // 添加到队列中
-        ReadBufferQueue.Enqueue(buffer);
+        callback?.Invoke(1);
         return Hex.ToHexString(resultBuffer);
     }
-
-    /// <summary>
-    /// 停止计算
-    /// </summary>
-    /// <param name="stream"></param>
-    public static void StopDigest(FileStream stream) => StoppingDigestSet.Add(stream);
 
     /// <summary>
     /// sha1 摘要
@@ -169,80 +145,98 @@ public class DataDigest {
     /// SHA1 摘要
     /// </summary>
     /// <param name="stream"></param>
-    /// <returns></returns>
-    public static string SHA1Digest(FileStream stream, Action<long>? callback = null) {
-        return GeneralDigest(stream, new Sha1Digest(), callback);
+    /// <param name="cancellationToken"></param>
+    /// <param name="callback">进度回调，参数为进度百分比</param>
+    /// <returns>任务取消返回 null</returns>
+    public static string? SHA1Digest(FileStream stream, CancellationToken? cancellationToken = null, Action<double>? callback = null) {
+        return GeneralDigest(stream, new Sha1Digest(), cancellationToken, callback);
     }
 
     /// <summary>
     /// sha3 摘要
     /// </summary>
     /// <param name="stream"></param>
-    /// <returns></returns>
-    public static string SHA3Digest(FileStream stream, Action<long>? callback = null) {
-        return GeneralDigest(stream, new Sha3Digest(), callback);
+    /// <param name="cancellationToken"></param>
+    /// <param name="callback">进度回调，参数为进度百分比</param>
+    /// <returns>任务取消返回 null</returns>
+    public static string? SHA3Digest(FileStream stream, CancellationToken? cancellationToken = null, Action<double>? callback = null) {
+        return GeneralDigest(stream, new Sha3Digest(), cancellationToken, callback);
     }
 
     /// <summary>
     /// sha224 摘要
     /// </summary>
     /// <param name="stream"></param>
-    /// <returns></returns>
-    public static string SHA224Digest(FileStream stream, Action<long>? callback = null) {
-        return GeneralDigest(stream, new Sha224Digest(), callback);
+    /// <param name="cancellationToken"></param>
+    /// <param name="callback">进度回调，参数为进度百分比</param>
+    /// <returns>任务取消返回 null</returns>
+    public static string? SHA224Digest(FileStream stream, CancellationToken? cancellationToken = null, Action<double>? callback = null) {
+        return GeneralDigest(stream, new Sha224Digest(), cancellationToken, callback);
     }
 
     /// <summary>
     /// sha256 摘要
     /// </summary>
     /// <param name="stream"></param>
-    /// <returns></returns>
-    public static string SHA256Digest(FileStream stream, Action<long>? callback = null) {
-        return GeneralDigest(stream, new Sha256Digest(), callback);
+    /// <param name="cancellationToken"></param>
+    /// <param name="callback">进度回调，参数为进度百分比</param>
+    /// <returns>任务取消返回 null</returns>
+    public static string? SHA256Digest(FileStream stream, CancellationToken? cancellationToken = null, Action<double>? callback = null) {
+        return GeneralDigest(stream, new Sha256Digest(), cancellationToken, callback);
     }
 
     /// <summary>
     /// sha384 摘要
     /// </summary>
     /// <param name="stream"></param>
-    /// <returns></returns>
-    public static string SHA384Digest(FileStream stream, Action<long>? callback = null) {
-        return GeneralDigest(stream, new Sha384Digest(), callback);
+    /// <param name="cancellationToken"></param>
+    /// <param name="callback">进度回调，参数为进度百分比</param>
+    /// <returns>任务取消返回 null</returns>
+    public static string? SHA384Digest(FileStream stream, CancellationToken? cancellationToken = null, Action<double>? callback = null) {
+        return GeneralDigest(stream, new Sha384Digest(), cancellationToken, callback);
     }
 
     /// <summary>
     /// sha512 摘要
     /// </summary>
     /// <param name="stream"></param>
-    /// <returns></returns>
-    public static string SHA512Digest(FileStream stream, Action<long>? callback = null) {
-        return GeneralDigest(stream, new Sha512Digest(), callback);
+    /// <param name="cancellationToken"></param>
+    /// <param name="callback">进度回调，参数为进度百分比</param>
+    /// <returns>任务取消返回 null</returns>
+    public static string? SHA512Digest(FileStream stream, CancellationToken? cancellationToken = null, Action<double>? callback = null) {
+        return GeneralDigest(stream, new Sha512Digest(), cancellationToken, callback);
     }
 
     /// <summary>
     /// md5 摘要
     /// </summary>
     /// <param name="stream"></param>
-    /// <returns></returns>
-    public static string MD5Digest(FileStream stream, Action<long>? callback = null) {
-        return GeneralDigest(stream, new MD5Digest(), callback);
+    /// <param name="cancellationToken"></param>
+    /// <param name="callback">进度回调，参数为进度百分比</param>
+    /// <returns>任务取消返回 null</returns>
+    public static string? MD5Digest(FileStream stream, CancellationToken? cancellationToken = null, Action<double>? callback = null) {
+        return GeneralDigest(stream, new MD5Digest(), cancellationToken, callback);
     }
 
     /// <summary>
     /// md4 摘要
     /// </summary>
     /// <param name="stream"></param>
-    /// <returns></returns>
-    public static string MD4Digest(FileStream stream, Action<long>? callback = null) {
-        return GeneralDigest(stream, new MD4Digest(), callback);
+    /// <param name="cancellationToken"></param>
+    /// <param name="callback">进度回调，参数为进度百分比</param>
+    /// <returns>任务取消返回 null</returns>
+    public static string? MD4Digest(FileStream stream, CancellationToken? cancellationToken = null, Action<double>? callback = null) {
+        return GeneralDigest(stream, new MD4Digest(), cancellationToken, callback);
     }
 
     /// <summary>
     /// md2 摘要
     /// </summary>
     /// <param name="stream"></param>
-    /// <returns></returns>
-    public static string MD2Digest(FileStream stream, Action<long>? callback = null) {
-        return GeneralDigest(stream, new MD2Digest(), callback);
+    /// <param name="cancellationToken"></param>
+    /// <param name="callback">进度回调，参数为进度百分比</param>
+    /// <returns>任务取消返回 null</returns>
+    public static string? MD2Digest(FileStream stream, CancellationToken? cancellationToken = null, Action<double>? callback = null) {
+        return GeneralDigest(stream, new MD2Digest(), cancellationToken, callback);
     }
 }
