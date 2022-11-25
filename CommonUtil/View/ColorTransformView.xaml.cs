1fd2785d82a531583ea8faddda7cbfe5395b08ab
Luxiu123 <klt5466@163.com>
2022-11-25 22:33:22 +0800

    mod: 修改 ColorTransformView

--- CommonUtil/View/ColorTransformView.xaml.cs
+++ CommonUtil/View/ColorTransformView.xaml.cs
@@ -11,6 +11,27 @@ using System.Windows.Media;
 namespace CommonUtil.View;
 
 public partial class ColorTransformView : Page {
+    public class SliderInfo : DependencyObject {
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(double), typeof(SliderInfo), new PropertyMetadata(0.0));
+
+        public double MinValue { get; }
+        public double MaxValue { get; }
+        public double SmallChange { get; }
+        /// <summary>
+        /// 当前值
+        /// </summary>
+        public double Value {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public SliderInfo(double minValue, double maxValue, double smallChange) {
+            MinValue = minValue;
+            MaxValue = maxValue;
+            SmallChange = smallChange;
+        }
+    }
+
     public class ColorItem : DependencyObject {
         public static readonly DependencyProperty ColorProperty = DependencyProperty.Register("Color", typeof(string), typeof(ColorItem), new PropertyMetadata(""));
 
@@ -21,25 +42,28 @@ public partial class ColorTransformView : Page {
         public string Tag { get; set; } = string.Empty;
         public Func<Color, string> ColorToString { get; set; }
         public Func<string, Color?> StringToColor { get; set; }
+        public Delegate SliderConverter { get; }
+        public IList<SliderInfo> Sliders { get; }
 
-        public ColorItem(string tag, Func<Color, string> colorToString, Func<string, Color?> stringToColor) {
+        public ColorItem(string tag, Func<Color, string> colorToString, Func<string, Color?> stringToColor, Delegate sliderConverter, IList<SliderInfo> sliders) {
             Tag = tag;
             ColorToString = colorToString;
             StringToColor = stringToColor;
+            SliderConverter = sliderConverter;
+            Sliders = sliders;
         }
 
     }
 
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-
     public static readonly DependencyProperty SelectedColorProperty = DependencyProperty.Register("SelectedColor", typeof(Color), typeof(ColorTransformView), new PropertyMetadata());
-    public static readonly DependencyProperty ColorItemsProperty = DependencyProperty.Register("ColorItems", typeof(IList<ColorItem>), typeof(ColorTransformView), new PropertyMetadata());
+    private static readonly DependencyProperty ColorItemsProperty = DependencyProperty.Register("ColorItems", typeof(IList<ColorItem>), typeof(ColorTransformView), new PropertyMetadata());
 
     public Color SelectedColor {
         get { return (Color)GetValue(SelectedColorProperty); }
         set { SetValue(SelectedColorProperty, value); }
     }
-    public IList<ColorItem> ColorItems {
+    private IList<ColorItem> ColorItems {
         get { return (IList<ColorItem>)GetValue(ColorItemsProperty); }
         set { SetValue(ColorItemsProperty, value); }
     }
@@ -50,17 +74,131 @@ public partial class ColorTransformView : Page {
     public ColorTransformView() {
         DependencyPropertyDescriptor.FromProperty(SelectedColorProperty, typeof(ColorTransformView)).AddValueChanged(this, ColorChangedHandler);
         ColorItems = new List<ColorItem>() {
-             new ("HEX", ColorTransform.ColorToHEX, ColorTransform.HEXToColor),
-             new ("RGB", ColorTransform.ColorToRGB, ColorTransform.RGBToColor),
-             new ("RGBA1", ColorTransform.ColorToRGBA1, ColorTransform.RGBA1ToColor),
-             new ("RGBA2", ColorTransform.ColorToRGBA2, ColorTransform.RGBA2ToColor),
-             new ("HSL", ColorTransform.ColorToHSL, ColorTransform.HSLToColor),
-             new ("HSV", ColorTransform.ColorToHSV, ColorTransform.HSVToColor),
-             new ("LAB", ColorTransform.ColorToLAB, ColorTransform.LABToColor),
-             new ("XYZ", ColorTransform.ColorToXYZ, ColorTransform.XYZToColor),
-             new ("LCH", ColorTransform.ColorToLCH, ColorTransform.LCHToColor),
-             new ("CMYK", ColorTransform.ColorToCMYK, ColorTransform.CMYKToColor),
-             new ("LUV", ColorTransform.ColorToLUV, ColorTransform.LUVToColor),
+             new (
+                 "HEX",
+                 ColorTransform.ColorToHEX,
+                 ColorTransform.HEXToColor,
+                 (Func<double,double,double,double,Color?>)ColorTransform.HEXToColor,
+                 new List<SliderInfo>(){
+                     new (0, 255, 1),
+                     new (0, 255, 1),
+                     new (0, 255, 1),
+                     new (0, 255, 1),
+                 }
+             ),
+             new (
+                 "RGB",
+                 ColorTransform.ColorToRGB,
+                 ColorTransform.RGBToColor,
+                 (Func<double,double,double,Color?>)ColorTransform.RGBToColor,
+                 new List<SliderInfo>(){
+                     new (0, 255, 1),
+                     new (0, 255, 1),
+                     new (0, 255, 1),
+                 }
+             ),
+             new (
+                 "RGBA1",
+                 ColorTransform.ColorToRGBA1,
+                 ColorTransform.RGBA1ToColor,
+                 (Func<double,double,double,double,Color?>)ColorTransform.RGBA1ToColor,
+                 new List<SliderInfo>(){
+                     new (0, 255, 1),
+                     new (0, 255, 1),
+                     new (0, 255, 1),
+                     new (0, 255, 1),
+                 }
+             ),
+             new (
+                 "RGBA2",
+                 ColorTransform.ColorToRGBA2,
+                 ColorTransform.RGBA2ToColor,
+                 (Func<double,double,double,double,Color?>)ColorTransform.RGBA2ToColor,
+                 new List<SliderInfo>(){
+                     new (0, 255, 1),
+                     new (0, 255, 1),
+                     new (0, 255, 1),
+                     new (0, 1, 0.01),
+                 }
+             ),
+             new (
+                 "HSL",
+                 ColorTransform.ColorToHSL,
+                 ColorTransform.HSLToColor,
+                 (Func<double, double, double, Color?>)ColorTransform.HSLToColor,
+                 new List<SliderInfo>(){
+                     new (0, 360, 1),
+                     new (0, 100, 1),
+                     new (0, 100, 1),
+                 }
+             ),
+             new (
+                 "HSV",
+                 ColorTransform.ColorToHSV,
+                 ColorTransform.HSVToColor,
+                 (Func<double, double, double, Color?>)ColorTransform.HSVToColor,
+                 new List<SliderInfo>(){
+                     new (0, 360, 1),
+                     new (0, 100, 1),
+                     new (0, 100, 1),
+                 }
+             ),
+             new (
+                 "LAB",
+                 ColorTransform.ColorToLAB,
+                 ColorTransform.LABToColor,
+                 (Func<double, double, double, Color?>)ColorTransform.LABToColor,
+                 new List<SliderInfo>(){
+                     new (0, 100, 1),
+                     new (-128, 127, 1),
+                     new (-128, 127, 1),
+                 }
+             ),
+             new (
+                 "XYZ",
+                 ColorTransform.ColorToXYZ,
+                 ColorTransform.XYZToColor,
+                 (Func<double, double, double, Color?>)ColorTransform.XYZToColor,
+                 new List<SliderInfo>(){
+                     new (0, 95.05, 1),
+                     new (0, 100, 1),
+                     new (0, 108.88, 1),
+                 }
+             ),
+             new (
+                 "LCH",
+                 ColorTransform.ColorToLCH,
+                 ColorTransform.LCHToColor,
+                 (Func<double, double, double, Color?>)ColorTransform.LCHToColor,
+                 new List<SliderInfo>(){
+                     new (0, 100, 1),
+                     new (0, 230, 1),
+                     new (0, 270, 1),
+                 }
+             ),
+             new (
+                 "CMYK",
+                 ColorTransform.ColorToCMYK,
+                 ColorTransform.CMYKToColor,
+                 (Func<double,double,double,double,Color?>)ColorTransform.CMYKToColor,
+                 new List<SliderInfo>(){
+                     new (0, 1, 0.01),
+                     new (0, 1, 0.01),
+                     new (0, 1, 0.01),
+                     new (0, 1, 0.01),
+                 }
+             ),
+             new (
+                 "LUV",
+                 ColorTransform.ColorToLUV,
+                 ColorTransform.LUVToColor,
+                 (Func<double, double, double, Color?>)ColorTransform.LUVToColor,
+                 new List<SliderInfo>(){
+                     new (0, 100, 1),
+                     new (-134, 220, 1),
+                     new (-140, 122, 1),
+                 }
+             ),
         };
         InitializeComponent();
         SelectedColor = Colors.White;
@@ -105,7 +243,6 @@ public partial class ColorTransformView : Page {
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
-    /// <exception cref="NotImplementedException"></exception>
     private void ColorChangedHandler(object? sender, EventArgs e) {
         ColorPanel.Fill = new SolidColorBrush(SelectedColor);
         IsColorChanged = true;
@@ -127,6 +264,9 @@ public partial class ColorTransformView : Page {
     /// <param name="e"></param>
     private void TextBoxKeyUpHandler(object sender, System.Windows.Input.KeyEventArgs e) {
         e.Handled = true;
+        if (e.Key != System.Windows.Input.Key.Enter) {
+            return;
+        }
         if (sender is TextBox box && box.DataContext is ColorItem colorItem) {
             UpdateInputColor(colorItem.StringToColor(colorItem.Color));
         }
@@ -145,4 +285,3 @@ public partial class ColorTransformView : Page {
         }
     }
 }
-
