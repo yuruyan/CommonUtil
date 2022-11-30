d60ace66f54d96c6fdb2f1d0a75c09c912d0d463
Luxiu123 <klt5466@163.com>
2022-11-30 11:10:13 +0800

    feat: 自适应 CommonRegexListDialog width

--- CommonUtil/View/Dialog/CommonRegexListDialog.xaml.cs
+++ CommonUtil/View/Dialog/CommonRegexListDialog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ModernWpf.Controls;
+﻿using CommonUITools.Utils;
+using ModernWpf.Controls;
 using Newtonsoft.Json;
 using NLog;
 using System.Collections.Generic;
@@ -9,8 +10,18 @@ namespace CommonUtil.View;
 
 public partial class CommonRegexListDialog : ContentDialog {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-
     private static readonly IEnumerable<KeyValuePair<string, string>> CommonRegexList;
+
+    /// <summary>
+    /// 正则列表
+    /// </summary>
+    public IEnumerable<KeyValuePair<string, string>> RegexList {
+        get { return (IEnumerable<KeyValuePair<string, string>>)GetValue(RegexListProperty); }
+        set { SetValue(RegexListProperty, value); }
+    }
+    public static readonly DependencyProperty RegexListProperty = DependencyProperty.Register("RegexList", typeof(IEnumerable<KeyValuePair<string, string>>), typeof(CommonRegexListDialog), new PropertyMetadata());
+    private readonly double RegexListItemsControlMinWidth;
+    private readonly double RegexListItemsControlMaxWidth;
 
     /// <summary>
     /// 加载 CommonRegexList
@@ -25,17 +36,29 @@ public partial class CommonRegexListDialog : ContentDialog {
         CommonRegexList = list;
     }
 
-    /// <summary>
-    /// 正则列表
-    /// </summary>
-    public IEnumerable<KeyValuePair<string, string>> RegexList {
-        get { return (IEnumerable<KeyValuePair<string, string>>)GetValue(RegexListProperty); }
-        set { SetValue(RegexListProperty, value); }
-    }
-    public static readonly DependencyProperty RegexListProperty = DependencyProperty.Register("RegexList", typeof(IEnumerable<KeyValuePair<string, string>>), typeof(CommonRegexListDialog), new PropertyMetadata());
-
     public CommonRegexListDialog() {
         RegexList = CommonRegexList;
         InitializeComponent();
+        RegexListItemsControlMinWidth = (double)Resources["RegexListItemsControlMinWidth"];
+        RegexListItemsControlMaxWidth = (double)Resources["RegexListItemsControlMaxWidth"];
+        // 动态更新 width
+        UIUtils.SetLoadedOnceEventHandler(this, (_, _) => {
+            var window = Window.GetWindow(this);
+            UpdateDialogWidth(window.ActualWidth / 2);
+            window.SizeChanged += (_, arg) => UpdateDialogWidth(arg.NewSize.Width / 2);
+        });
+    }
+
+    /// <summary>
+    /// 更新 RegexListItemsControl Width
+    /// </summary>
+    /// <param name="newWidth"></param>
+    private void UpdateDialogWidth(double newWidth) {
+        if (newWidth < RegexListItemsControlMinWidth) {
+            newWidth = RegexListItemsControlMinWidth;
+        } else if (newWidth > RegexListItemsControlMaxWidth) {
+            newWidth = RegexListItemsControlMaxWidth;
+        }
+        RegexListItemsControl.Width = newWidth;
     }
 }
