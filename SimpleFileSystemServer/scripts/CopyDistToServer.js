e90dbf9bb8e6f1cad9786d9dc08d97317959c6e4
Luxiu123 <klt5466@163.com>
2022-12-04 11:15:25 +0800

    mod: 添加部署脚本

---
+++ SimpleFileSystemServer/scripts/CopyDistToServer.js
@@ -0,0 +1,14 @@
+﻿/**
+ * 拷贝前端打包的 dist 文件夹
+ * */
+
+import * as fs from 'fs'
+
+const distFolder = './../../SimpleFileSystemServer-FrontEnd/dist'
+// 复制 index.html 为 index.cshtml
+fs.copyFileSync(`${distFolder}/index.html`, './../views/index.cshtml')
+console.log('copy "index.html" done')
+// 复制 assets 文件夹到 wwwroot
+fs.rmSync(`./../wwwroot/assets`, { recursive: true })
+fs.cpSync(`${distFolder}/assets`, './../wwwroot/assets', { recursive: true })
+console.log('copy "assets" to "wwwroot" done')
