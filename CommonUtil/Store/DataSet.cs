f1a656d31c9c9ad7fe6d2ac6091c0ed82ecd8981
Luxiu123 <klt5466@163.com>
2022-12-06 22:09:24 +0800

    feat: 添加 DataSet 数据集

---
+++ CommonUtil/Store/DataSet.cs
@@ -0,0 +1,17 @@
+﻿namespace CommonUtil.Store;
+
+/// <summary>
+/// 数据集
+/// </summary>
+public static class DataSet {
+    /// <summary>
+    /// 代理类型
+    /// </summary>
+    public static readonly IReadOnlyList<string> ProxyTypes = new List<string>() {
+        "http",
+        "https",
+        "socks4",
+        "socks4a",
+        "socks5",
+    };
+}
