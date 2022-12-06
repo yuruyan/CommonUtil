1570c6ef830c20e2374037a0ccf5199f0403f64c
Luxiu123 <klt5466@163.com>
2022-12-06 15:05:44 +0800

    feat: 添加 FileProcessUtils

---
+++ CommonUtil/Model/Delegates.cs
@@ -0,0 +1,15 @@
+﻿namespace CommonUtil.Model;
+
+public delegate void CommonFileProcess(
+    string inputFile,
+    string outputFile,
+    CancellationToken? token,
+    Action<double>? callback
+);
+
+public delegate T CommonFileProcess<T>(
+    string inputFile,
+    string outputFile,
+    CancellationToken? token,
+    Action<double>? callback
+);
---
+++ CommonUtil/Utils/FileProcessUtils.cs
@@ -0,0 +1,244 @@
+﻿using CommonUtil.Store;
+using Microsoft.Win32;
+using Ookii.Dialogs.Wpf;
+
+namespace CommonUtil.Utils;
+
+/// <summary>
+/// 文件处理工具
+/// </summary>
+public static class FileProcessUtils {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    /// <summary>
+    /// 处理多文件任务
+    /// </summary>
+    /// <param name="sourceFilenames">源文件列表</param>
+    /// <param name="saveDirectoryDialog">保存文件夹 Dialog</param>
+    /// <param name="source">CancellationTokenSource</param>
+    /// <param name="fileProcessStatuses">进度列表</param>
+    /// <param name="processFunc">处理方法，参数列表为 (inputFile, outputFile, FileProcessStatus)</param>
+    /// <param name="dispatcher">Dispatcher，默认为 Application.Current.Dispatcher</param>
+    /// <param name="parentWindow">所属 Window，默认为 Application.Current.MainWindow</param>
+    /// <param name="log">日志，默认为 <see cref="Logger"/></param>
+    /// <returns>Task</returns>
+    [NoException]
+    public static async Task ProcessMultiFilesAsync(
+         ICollection<string> sourceFilenames,
+         VistaFolderBrowserDialog saveDirectoryDialog,
+         CancellationTokenSource source,
+         ObservableCollection<FileProcessStatus> fileProcessStatuses,
+         Action<string, string, FileProcessStatus> processFunc,
+         Dispatcher? dispatcher = null,
+         Window? parentWindow = null,
+         Logger? log = null
+    ) {
+        dispatcher ??= Application.Current.Dispatcher;
+        parentWindow ??= Application.Current.MainWindow;
+        log ??= Logger;
+        // 选择保存目录
+        if (saveDirectoryDialog.ShowDialog(parentWindow) != true) {
+            return;
+        }
+        var saveDirectory = saveDirectoryDialog.SelectedPath;
+        var tasks = new List<Task>();
+        int chunkSize = (int)Math.Ceiling(sourceFilenames.Count / (double)Global.ConcurrentTaskCount);
+        // 分配任务运行
+        foreach (var files in sourceFilenames.Chunk(chunkSize)) {
+            tasks.Add(Task.Run(() => {
+                foreach (var file in files) {
+                    // 任务取消
+                    if (source.IsCancellationRequested) {
+                        log.Info("任务取消");
+                        return;
+                    }
+                    var outputFile = Path.Combine(saveDirectory, Path.GetFileName(file));
+                    var status = dispatcher.Invoke(() => {
+                        var status = new FileProcessStatus() {
+                            FileName = outputFile,
+                            Status = ProcessResult.Processing
+                        };
+                        fileProcessStatuses.Add(status);
+                        return status;
+                    });
+
+                    try {
+                        processFunc(file, outputFile, status);
+                        ProcessStatusUtils.UpdateProcessStatusWhenCompleted(
+                            status,
+                            source.Token,
+                            new FileInfo(outputFile).Length
+                        );
+                    } catch (Exception error) {
+                        log.Error(error);
+                        ProcessStatusUtils.UpdateProcessStatusWhenCompleted(status, ProcessResult.Failed);
+                    }
+                }
+            }, source.Token));
+        }
+        await Task.WhenAll(tasks);
+    }
+
+    /// <param name="processFunc">处理方法</param>
+    /// <inheritdoc cref="ProcessMultiFilesAsync(ICollection{string}, VistaFolderBrowserDialog, CancellationTokenSource, ObservableCollection{FileProcessStatus}, Action{string, string, FileProcessStatus}, Dispatcher?, Window?, Logger?)"/>
+    public static async Task ProcessMultiFilesAsync(
+        ICollection<string> sourceFilenames,
+        VistaFolderBrowserDialog saveDirectoryDialog,
+        CancellationTokenSource source,
+        ObservableCollection<FileProcessStatus> fileProcessStatuses,
+        CommonFileProcess processFunc,
+        Dispatcher? dispatcher = null,
+        Window? parentWindow = null,
+        Logger? log = null
+     ) {
+        await ProcessMultiFilesAsync(
+            sourceFilenames,
+            saveDirectoryDialog,
+            source,
+            fileProcessStatuses,
+            (inputFile, outputFile, status) => processFunc(
+                inputFile,
+                outputFile,
+                source.Token,
+                proc => ThrottleUtils.Throttle(status, () => {
+                    ProcessStatusUtils.UpdateProcessStatus(status, proc);
+                })
+            ),
+            dispatcher,
+            parentWindow,
+            log
+        );
+    }
+
+    /// <inheritdoc cref="ProcessMultiFilesAsync(ICollection{string}, VistaFolderBrowserDialog, CancellationTokenSource, ObservableCollection{FileProcessStatus}, CommonFileProcess, Dispatcher?, Window?, Logger?)"/>
+    public static async Task ProcessMultiFilesAsync<T>(
+        ICollection<string> sourceFilenames,
+        VistaFolderBrowserDialog saveDirectoryDialog,
+        CancellationTokenSource source,
+        ObservableCollection<FileProcessStatus> fileProcessStatuses,
+        CommonFileProcess<T> processFunc,
+        Dispatcher? dispatcher = null,
+        Window? parentWindow = null,
+        Logger? log = null
+     ) {
+        await ProcessMultiFilesAsync(
+            sourceFilenames,
+            saveDirectoryDialog,
+            source,
+            fileProcessStatuses,
+            (inputFile, outputFile, token, callback) => {
+                processFunc(inputFile, outputFile, token, callback);
+            },
+            dispatcher,
+            parentWindow,
+            log
+        );
+    }
+
+    /// <summary>
+    /// 处理单个文件
+    /// </summary>
+    /// <param name="sourceFilename">源文件</param>
+    /// <param name="saveFileDialog">保存文件 Dialog</param>
+    /// <param name="source">CancellationTokenSource</param>
+    /// <param name="fileProcessStatuses">进度列表</param>
+    /// <param name="processFunc">处理方法，参数列表为 (inputFile, outputFile, FileProcessStatus)</param>
+    /// <param name="log">日志，默认为 <see cref="Logger"/></param>
+    /// <returns>Task</returns>
+    [NoException]
+    public static async Task ProcessOneFileAsync(
+        string sourceFilename,
+        SaveFileDialog saveFileDialog,
+        CancellationTokenSource source,
+        ObservableCollection<FileProcessStatus> fileProcessStatuses,
+        Action<string, string, FileProcessStatus> processFunc,
+        Logger? log = null
+    ) {
+        log ??= Logger;
+        saveFileDialog.FileName = Path.GetFileName(sourceFilename);
+        if (saveFileDialog.ShowDialog() != true) {
+            return;
+        }
+        var saveFilename = saveFileDialog.FileName;
+        var status = new FileProcessStatus() {
+            FileName = saveFilename,
+            Status = ProcessResult.Processing,
+        };
+        fileProcessStatuses.Add(status);
+
+        // 处理
+        try {
+            await Task.Run(
+                () => processFunc(sourceFilename, saveFilename, status),
+                source.Token
+            );
+            // 任务取消
+            if (source.IsCancellationRequested) {
+                status.Status = ProcessResult.Interrupted;
+            }
+            // 通知
+            else {
+                status.Status = ProcessResult.Successful;
+                status.Process = 1;
+                status.FileSize = new FileInfo(saveFilename).Length;
+                UIUtils.NotificationOpenFileInExplorerAsync(saveFilename, title: "处理成功");
+            }
+        } catch (IOException error) {
+            status.Status = ProcessResult.Failed;
+            log.Error(error);
+            MessageBox.Error("文件读取或保存失败");
+        } catch (Exception error) {
+            status.Status = ProcessResult.Failed;
+            log.Error(error);
+            MessageBox.Error("处理失败");
+        }
+    }
+
+    /// <param name="processFunc">处理方法</param>
+    /// <inheritdoc cref="ProcessOneFileAsync(string, SaveFileDialog, CancellationTokenSource, ObservableCollection{FileProcessStatus}, Action{string, string, FileProcessStatus}, Logger?)"/>
+    public static async Task ProcessOneFileAsync(
+        string sourceFilename,
+        SaveFileDialog saveFileDialog,
+        CancellationTokenSource source,
+        ObservableCollection<FileProcessStatus> fileProcessStatuses,
+        CommonFileProcess processFunc,
+        Logger? log = null
+    ) {
+        await ProcessOneFileAsync(
+            sourceFilename,
+            saveFileDialog,
+            source,
+            fileProcessStatuses,
+            (inputFile, outputFile, status) => processFunc(
+                inputFile,
+                outputFile,
+                source.Token,
+                proc => ThrottleUtils.Throttle(status, () => {
+                    ProcessStatusUtils.UpdateProcessStatus(status, proc);
+                })
+            ),
+            log
+        );
+    }
+
+    /// <inheritdoc cref="ProcessOneFileAsync(string, SaveFileDialog, CancellationTokenSource, ObservableCollection{FileProcessStatus}, CommonFileProcess, Logger?)"/>
+    public static async Task ProcessOneFileAsync<T>(
+        string sourceFilename,
+        SaveFileDialog saveFileDialog,
+        CancellationTokenSource source,
+        ObservableCollection<FileProcessStatus> fileProcessStatuses,
+        CommonFileProcess<T> processFunc,
+        Logger? log = null
+    ) {
+        await ProcessOneFileAsync(
+            sourceFilename,
+            saveFileDialog,
+            source,
+            fileProcessStatuses,
+            (inputFile, outputFile, token, callback) => {
+                processFunc(inputFile, outputFile, token, callback);
+            },
+            log
+        );
+    }
+}
