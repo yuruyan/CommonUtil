52678ae9c376fe508581d15c4e2815f9bef90ecd
Luxiu123 <klt5466@163.com>
2023-03-01 15:48:43 +0800

    add DesktopAutomation

---
+++ CommonUtil.Core/Core/DesktopAutomation.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonUtil.Core.Core;
+
+public static class DesktopAutomation {
+    public static async Task InputTextAsync(string text) {
+        await WindowsInput.Simulate
+            .Events()
+            .Click(text)
+            .Invoke();
+    }
+}
---
+++ CommonUtil/View/DesktopAutomation.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CommonUtil.View;
+
+public partial class DesktopAutomation : Page {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+    public DesktopAutomation() {
+        InitializeComponent();
+    }
+}
