b40cf5def9c14073fb37bfe1547fc1d15d4adc8c
Luxiu123 <klt5466@163.com>
2023-03-02 22:41:22 +0800

    Update PressKeyShortcutDialog

--- CommonUtil/View/DesktopAutomation/PressKeyShortcutDialog.xaml.cs
+++ CommonUtil/View/DesktopAutomation/PressKeyShortcutDialog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using CommonUITools.View;
+﻿using ModernWpf.Controls;
+using WindowsInput.Events;
 
 namespace CommonUtil.View;
 
@@ -6,6 +7,20 @@ public partial class PressKeyShortcutDialog : DesktopAutomationDialog {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
     public PressKeyShortcutDialog() {
+        AutomationMethod = DesktopAutomation.PressKeyShortcut;
+        DescriptionHeader = "按下快捷键";
         InitializeComponent();
+    }
+
+    /// <summary>
+    /// Set values
+    /// </summary>
+    /// <param name="dialog"></param>
+    /// <param name="e"></param>
+    private void ClosingHandler(ContentDialog dialog, ContentDialogClosingEventArgs e) {
+        Parameters = new object[] {
+            new KeyCode[] { KeyCode.None }
+        };
+        DescriptionValue = "";
     }
 }
