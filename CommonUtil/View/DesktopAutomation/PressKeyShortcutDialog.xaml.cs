12058b0ab2f49ea8cc247bfabba4d6b4abadc6b3
Luxiu123 <klt5466@163.com>
2023-03-03 16:58:56 +0800

    Update PressKeyShortcutDialog

--- CommonUtil/View/DesktopAutomation/PressKeyShortcutDialog.xaml.cs
+++ CommonUtil/View/DesktopAutomation/PressKeyShortcutDialog.xaml.cs
@@ -4,9 +4,32 @@ using WindowsInput.Events;
 namespace CommonUtil.View;
 
 public partial class PressKeyShortcutDialog : DesktopAutomationDialog {
+    public class KeyStrokeItem : DependencyObject {
+        public static readonly DependencyProperty CodeProperty = DependencyProperty.Register("Code", typeof(string), typeof(KeyStrokeItem), new PropertyMetadata(KeyCode.None.ToString()));
+
+        public string Code {
+            get { return (string)GetValue(CodeProperty); }
+            set { SetValue(CodeProperty, value); }
+        }
+    }
+
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+    public static readonly DependencyProperty KeyCodeListProperty = DependencyProperty.Register("KeyCodeList", typeof(ExtendedObservableCollection<KeyStrokeItem>), typeof(PressKeyShortcutDialog), new PropertyMetadata());
+    internal static readonly DependencyPropertyKey KeyCodeNameListPropertyKey = DependencyProperty.RegisterReadOnly("KeyCodeNameList", typeof(string[]), typeof(PressKeyShortcutDialog), new PropertyMetadata());
+    public static readonly DependencyProperty KeyCodeNameListProperty = KeyCodeNameListPropertyKey.DependencyProperty;
+
+    public string[] KeyCodeNameList => (string[])GetValue(KeyCodeNameListProperty);
+    /// <summary>
+    /// 按键集合
+    /// </summary>
+    public ExtendedObservableCollection<KeyStrokeItem> KeyCodeList {
+        get { return (ExtendedObservableCollection<KeyStrokeItem>)GetValue(KeyCodeListProperty); }
+        set { SetValue(KeyCodeListProperty, value); }
+    }
 
     public PressKeyShortcutDialog() {
+        SetValue(KeyCodeNameListPropertyKey, Enum.GetNames<KeyCode>());
+        KeyCodeList = new();
         AutomationMethod = DesktopAutomation.PressKeyShortcut;
         Title = DescriptionHeader = "按下快捷键";
         InitializeComponent();
@@ -21,12 +44,41 @@ public partial class PressKeyShortcutDialog : DesktopAutomationDialog {
         _ = dialog;
         _ = e;
         Parameters = new object[] {
-            new KeyCode[] { KeyCode.None }
+            KeyCodeList.Select(
+                item => Enum.Parse<KeyCode>(item.Code)
+            ).ToArray(),
         };
-        DescriptionValue = "";
+        DescriptionValue = string.Join('+', (KeyCode[])Parameters[0]);
     }
 
     public override void ParseParameters(object[] parameters) {
+        KeyCodeList = new(((KeyCode[])parameters[0]).Select(
+            code => new KeyStrokeItem() { Code = code.ToString(), }
+        ));
+    }
 
+    /// <summary>
+    /// 添加按键
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    private void AddKeyStrokeClickHandler(ContentDialog sender, ContentDialogButtonClickEventArgs args) {
+        _ = sender;
+        _ = args;
+        args.Cancel = true;
+        KeyCodeList.Add(new());
+    }
+
+    /// <summary>
+    /// 设置 ItemsSource
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void KeyCodeListItemLoadedHandler(object sender, RoutedEventArgs e) {
+        if (sender is ComboBox comboBox) {
+            TaskUtils.EnsureCalledOnce(sender, () => {
+                comboBox.ItemsSource = KeyCodeNameList;
+            });
+        }
     }
 }
