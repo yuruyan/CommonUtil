efc28d17e914bc03718c3baddc6837735be896f9
Luxiu123 <klt5466@163.com>
2023-03-05 21:20:19 +0800

    Add PInvoke

---
+++ CommonUtil.Core/Util/PInvoke.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CommonUtil.Core.Util;
+
+internal static partial class PInvoke {
+    /// <summary>
+    /// Retrieves or sets the value of one of the system-wide parameters. This function can also update the user profile while setting a parameter.
+    /// </summary>
+    /// <param name="uAction"></param>
+    /// <param name="uParam"></param>
+    /// <param name="lpvParam"></param>
+    /// <param name="init"></param>
+    /// <returns></returns>
+    /// <see cref="https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-systemparametersinfoa"/>
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    public static extern bool SystemParametersInfo(uint uAction, uint uParam, StringBuilder lpvParam, uint init);
+}
---
+++ CommonUtil.Core/Util/PInvokeUtils.cs
@@ -0,0 +1,19 @@
+﻿namespace CommonUtil.Core.Util;
+
+public static class PInvokeUtils {
+    const uint SPI_GETDESKWALLPAPER = 0x0073;
+
+    /// <summary>
+    /// 获取桌面壁纸路径
+    /// </summary>
+    /// <returns>失败返回 null</returns>
+    public static string? GetDesktopWallpaper() {
+        var wallPaperPath = new StringBuilder(byte.MaxValue);
+        return PInvoke.SystemParametersInfo(
+            SPI_GETDESKWALLPAPER,
+            byte.MaxValue,
+            wallPaperPath,
+            0
+        ) ? wallPaperPath.ToString() : null;
+    }
+}
