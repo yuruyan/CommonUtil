9cb848025eab868da775580db1ceb05b0b3c24d7
Luxiu123 <klt5466@163.com>
2023-03-15 23:45:17 +0800

    Update ToolMenuItem model

--- CommonUtil/Model/ToolMenuItem.cs
+++ CommonUtil/Model/ToolMenuItem.cs
@@ -1,4 +1,6 @@
-﻿namespace CommonUtil.Model;
+﻿using AutoMapper;
+
+namespace CommonUtil.Model;
 
 public readonly record struct ToolMenuItem {
     public string Name { get; init; }
@@ -9,5 +11,43 @@ public readonly record struct ToolMenuItem {
         Name = name;
         ImagePath = imagePath;
         ClassType = classType;
+    }
+}
+
+public class ToolMenuItemDO : DependencyObject {
+    public static readonly DependencyProperty NameProperty = DependencyProperty.Register("Name", typeof(string), typeof(ToolMenuItemDO), new PropertyMetadata(string.Empty));
+    public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(string), typeof(ToolMenuItemDO), new PropertyMetadata());
+
+    public Type ViewType { get; }
+    /// <summary>
+    /// 菜单名称
+    /// </summary>
+    public string Name {
+        get { return (string)GetValue(NameProperty); }
+        set { SetValue(NameProperty, value); }
+    }
+    /// <summary>
+    /// 图标
+    /// </summary>
+    public string Icon {
+        get { return (string)GetValue(IconProperty); }
+        set { SetValue(IconProperty, value); }
+    }
+
+    public ToolMenuItemDO(Type viewType) {
+        ViewType = viewType;
+    }
+}
+
+public class ToolMenuItemProfile : Profile {
+    public ToolMenuItemProfile() {
+        CreateMap<ToolMenuItem, ToolMenuItem>();
+        CreateMap<ToolMenuItem, ToolMenuItemDO>()
+            .ConstructUsing(src => new ToolMenuItemDO(src.ClassType))
+            .ForMember(dest => dest.Icon, cfg => cfg.MapFrom(src => src.ImagePath));
+        CreateMap<ToolMenuItemDO, ToolMenuItemDO>();
+        CreateMap<ToolMenuItemDO, ToolMenuItem>()
+            .ForMember(dest => dest.ClassType, cfg => cfg.MapFrom(src => src.ViewType))
+            .ForMember(dest => dest.ImagePath, cfg => cfg.MapFrom(src => src.Icon));
     }
 }
---
+++ CommonUtil/Utils/MapperUtils.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+
+namespace CommonUtil.Utils;
+
+public class MapperUtils {
+    public static IMapper Instance => CommonUtils.GetSingletonInstance<IMapper>(typeof(MapperUtils), () => {
+        return new MapperConfiguration(cfg => {
+            cfg.ClearPrefixes();
+            cfg.SourceMemberNamingConvention = new LowerUnderscoreNamingConvention();
+            cfg.DestinationMemberNamingConvention = new PascalCaseNamingConvention();
+            cfg.AddMaps(typeof(ToolMenuItemProfile));
+        }).CreateMapper();
+    });
+
+    /// <summary>
+    /// 包装 Dispatcher.Invoke Map 方法
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public static T Map<T>(object obj) => UIUtils.RunOnUIThread(() => Instance.Map<T>(obj));
+}
