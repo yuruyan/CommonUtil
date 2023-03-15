f6f072cc66f024ec77086f6be40fafef904bcd66
Luxiu123 <klt5466@163.com>
2023-03-15 23:45:40 +0800

    Add NavigationContentView

---
+++ CommonUtil/View/Navigation/NavigationContentListView.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace CommonUtil.View.Navigation;
+
+public partial class NavigationContentListView : UserControl {
+    public static readonly DependencyProperty ToolMenuItemsProperty = DependencyProperty.Register("ToolMenuItems", typeof(ExtendedObservableCollection<ToolMenuItemDO>), typeof(NavigationContentListView), new PropertyMetadata());
+    /// <summary>
+    /// 选中项改变，参数为 ViewType
+    /// </summary>
+    public event EventHandler<Type>? SelectedMenuChanged;
+    /// <summary>
+    /// 关闭页面，参数为 ViewType
+    /// </summary>
+    public event EventHandler<Type>? Closed;
+
+    public ExtendedObservableCollection<ToolMenuItemDO> ToolMenuItems {
+        get { return (ExtendedObservableCollection<ToolMenuItemDO>)GetValue(ToolMenuItemsProperty); }
+        set { SetValue(ToolMenuItemsProperty, value); }
+    }
+
+    public NavigationContentListView() {
+        InitializeComponent();
+    }
+
+    /// <summary>
+    /// 关闭页面
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void ClosePageClickHandler(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        var menuItem = sender.GetElementDataContext<ToolMenuItemDO>();
+        if (menuItem is not null) {
+            ToolMenuItems.Remove(menuItem);
+            Closed?.Invoke(sender, menuItem.ViewType);
+        }
+    }
+
+    /// <summary>
+    /// 选中项变化
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void MenuItemSelectionChangedHandler(object sender, SelectionChangedEventArgs e) {
+        e.Handled = true;
+        var item = e.AddedItems.OfType<ToolMenuItemDO>().FirstOrDefault();
+        if (item is not null) {
+            SelectedMenuChanged?.Invoke(sender, item.ViewType);
+        }
+    }
+}
---
+++ CommonUtil/View/Navigation/NavigationContentView.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace CommonUtil.View;
+
+public partial class NavigationContentView : Page {
+    internal static readonly DependencyPropertyKey ToolMenuItemsPropertyKey = DependencyProperty.RegisterReadOnly("ToolMenuItems", typeof(ExtendedObservableCollection<ToolMenuItemDO>), typeof(NavigationContentView), new PropertyMetadata());
+    public static readonly DependencyProperty ToolMenuItemsProperty = ToolMenuItemsPropertyKey.DependencyProperty;
+
+    public ExtendedObservableCollection<ToolMenuItemDO> ToolMenuItems => (ExtendedObservableCollection<ToolMenuItemDO>)GetValue(ToolMenuItemsProperty);
+
+    public NavigationContentView() {
+        SetValue(ToolMenuItemsPropertyKey, new ExtendedObservableCollection<ToolMenuItemDO>());
+        InitializeComponent();
+    }
+
+    private void SelectedMenuChangedHandler(object sender, Type e) {
+        Console.WriteLine(e.Name);
+    }
+
+    private void PageClosedHandler(object sender, Type e) {
+        Console.WriteLine(e.Name);
+        if (ToolMenuItems.Count == 0) {
+            Console.WriteLine("empty list");
+        }
+    }
+}
+
