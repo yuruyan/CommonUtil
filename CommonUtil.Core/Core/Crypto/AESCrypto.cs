565021ebddf937e1ed3fc34de7bdce3de5a7579e
Luxiu123 <klt5466@163.com>
2023-03-27 16:15:43 +0800

    Add AESCryptoUtils

---
+++ CommonUtil.Core/Core/Crypto/AESCrypto.cs
@@ -0,0 +1,62 @@
+﻿using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace CommonUtil.Core;
+
+public static class AESCryptoUtils {
+    /// <summary>
+    /// AES 加密
+    /// </summary>    
+    /// <inheritdoc cref="DoCrypto(AESCryptoMode, AESPaddingMode, bool, byte[], byte[], byte[]?)"/>
+    public static byte[] Encrypt(
+        AESCryptoMode cryptoMode,
+        AESPaddingMode paddingMode,
+        byte[] data,
+        byte[] key,
+        byte[]? iv = null
+    ) {
+        return DoCrypto(cryptoMode, paddingMode, true, data, key, iv);
+    }
+
+    /// <summary>
+    /// AES 解密
+    /// </summary>
+    /// <inheritdoc cref="DoCrypto(AESCryptoMode, AESPaddingMode, bool, byte[], byte[], byte[]?)"/>
+    public static byte[] Decrypt(
+        AESCryptoMode cryptoMode,
+        AESPaddingMode paddingMode,
+        byte[] data,
+        byte[] key,
+        byte[]? iv = null
+    ) {
+        return DoCrypto(cryptoMode, paddingMode, false, data, key, iv);
+    }
+
+    /// <summary>
+    /// AES 加密 / 解密
+    /// </summary>
+    /// <param name="cryptoMode">加密模式</param>
+    /// <param name="paddingMode">填充模式</param>
+    /// <param name="isEncryption">加密 / 解密</param>
+    /// <param name="data">数据</param>
+    /// <param name="key">key, must be 16, 24 or 32 long</param>
+    /// <param name="iv">iv, must be 16 long</param>
+    /// <returns></returns>
+    public static byte[] DoCrypto(
+        AESCryptoMode cryptoMode,
+        AESPaddingMode paddingMode,
+        bool isEncryption,
+        byte[] data,
+        byte[] key,
+        byte[]? iv = null
+    ) {
+        var keyParameter = ParameterUtilities.CreateKeyParameter("AES", key);
+        var inCipher = CipherUtilities.GetCipher($"AES/{cryptoMode}/{paddingMode}");
+        if (iv is null) {
+            inCipher.Init(isEncryption, keyParameter);
+        } else {
+            inCipher.Init(isEncryption, new ParametersWithIV(keyParameter, iv));
+        }
+        return inCipher.DoFinal(data);
+    }
+}
---
+++ CommonUtil.Core/Model/AESMode.cs
@@ -0,0 +1,21 @@
+﻿namespace CommonUtil.Core;
+
+public enum AESPaddingMode {
+    NoPadding,
+    PKCS7Padding,
+    ISO10126Padding,
+}
+
+public enum AESCryptoMode {
+    /// <summary>
+    /// Must not with iv
+    /// </summary>
+    //ECB,
+    CBC,
+    CFB,
+    OFB,
+    /// <summary>
+    /// Must with iv
+    /// </summary>
+    CTR,
+}
