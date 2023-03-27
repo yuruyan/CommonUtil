f57a4c4d01f83ee4fceeace703f52b690625d2c1
Luxiu123 <klt5466@163.com>
2023-03-27 19:16:14 +0800

    Update images

--- CommonUtil/Store/ImagePath.cs
+++ CommonUtil/Store/ImagePath.cs
@@ -4,4 +4,9 @@ internal static class ImagePath {
     public static readonly Uri JapaneseSymbolImageUri = new($"{Global.ImageSource}JapaneseSymbol.png", UriKind.Relative);
     public static readonly Uri UTF8ImageUri = new($"{Global.ImageSource}UTF8Encoding.png", UriKind.Relative);
     public static readonly Uri Base64ImageUri = new($"{Global.ImageSource}base64.png", UriKind.Relative);
+    public static readonly Uri WIFIImageUri = new($"{Global.ImageSource}WIFI.png", UriKind.Relative);
+    public static readonly Uri LocationImageUri = new($"{Global.ImageSource}Location.png", UriKind.Relative);
+    public static readonly Uri MessageImageUri = new($"{Global.ImageSource}Message.png", UriKind.Relative);
+    public static readonly Uri GMailImageUri = new($"{Global.ImageSource}GMail.png", UriKind.Relative);
+    public static readonly Uri PhoneCallImageUri = new($"{Global.ImageSource}PhoneCall.png", UriKind.Relative);
 }
