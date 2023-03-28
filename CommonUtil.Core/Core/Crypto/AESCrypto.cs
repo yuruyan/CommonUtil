ae652d348e1da42627b08bbce915d685bb1a5012
Luxiu123 <klt5466@163.com>
2023-03-28 20:44:39 +0800

    Add file process

--- CommonUtil.Core/Core/Crypto/AESCrypto.cs
+++ CommonUtil.Core/Core/Crypto/AESCrypto.cs
@@ -5,6 +5,11 @@ namespace CommonUtil.Core;
 
 public static class AESCryptoUtils {
     /// <summary>
+    /// 默认读取缓冲区大小
+    /// </summary>
+    private const int ReadBuffer = 8192;
+
+    /// <summary>
     /// AES 加密
     /// </summary>    
     /// <inheritdoc cref="DoCrypto(AESCryptoMode, AESPaddingMode, bool, byte[], byte[], byte[]?)"/>
@@ -33,6 +38,44 @@ public static class AESCryptoUtils {
     }
 
     /// <summary>
+    /// 文件加密
+    /// </summary>
+    /// <inheritdoc cref="Encrypt(AESCryptoMode, AESPaddingMode, byte[], byte[], byte[]?)"/>
+    /// <param name="inputPath">要加密的文件</param>
+    /// <param name="outputPath">加密后的文件</param>
+    public static void EncryptFile(
+        AESCryptoMode cryptoMode,
+        AESPaddingMode paddingMode,
+        string inputPath,
+        string outputPath,
+        byte[] key,
+        byte[]? iv = null
+    ) {
+        using var readStream = File.OpenRead(inputPath);
+        using var writeStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write);
+        DoCrypto(cryptoMode, paddingMode, true, readStream, writeStream, key, iv);
+    }
+
+    /// <summary>
+    /// 文件解密
+    /// </summary>
+    /// <inheritdoc cref="Decrypt(AESCryptoMode, AESPaddingMode, byte[], byte[], byte[]?)"/>
+    /// <param name="inputPath">要解密的文件</param>
+    /// <param name="outputPath">解密后的文件</param>
+    public static void DecryptFile(
+        AESCryptoMode cryptoMode,
+        AESPaddingMode paddingMode,
+        string inputPath,
+        string outputPath,
+        byte[] key,
+        byte[]? iv = null
+    ) {
+        using var readStream = File.OpenRead(inputPath);
+        using var writeStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write);
+        DoCrypto(cryptoMode, paddingMode, false, readStream, writeStream, key, iv);
+    }
+
+    /// <summary>
     /// AES 加密 / 解密
     /// </summary>
     /// <param name="cryptoMode">加密模式</param>
@@ -42,7 +85,7 @@ public static class AESCryptoUtils {
     /// <param name="key">key, must be 16, 24 or 32 long</param>
     /// <param name="iv">iv, must be 16 long</param>
     /// <returns></returns>
-    public static byte[] DoCrypto(
+    private static byte[] DoCrypto(
         AESCryptoMode cryptoMode,
         AESPaddingMode paddingMode,
         bool isEncryption,
@@ -51,12 +94,43 @@ public static class AESCryptoUtils {
         byte[]? iv = null
     ) {
         var keyParameter = ParameterUtilities.CreateKeyParameter("AES", key);
-        var inCipher = CipherUtilities.GetCipher($"AES/{cryptoMode}/{paddingMode}");
+        var cipher = CipherUtilities.GetCipher($"AES/{cryptoMode}/{paddingMode}");
         if (iv is null) {
-            inCipher.Init(isEncryption, keyParameter);
+            cipher.Init(isEncryption, keyParameter);
         } else {
-            inCipher.Init(isEncryption, new ParametersWithIV(keyParameter, iv));
+            cipher.Init(isEncryption, new ParametersWithIV(keyParameter, iv));
         }
-        return inCipher.DoFinal(data);
+        return cipher.DoFinal(data);
+    }
+
+    /// <inheritdoc cref="DoCrypto(AESCryptoMode, AESPaddingMode, bool, byte[], byte[], byte[]?)"/>
+    /// <param name="inputStream">输入流</param>
+    /// <param name="outputStream">输出流</param>
+    private static void DoCrypto(
+        AESCryptoMode cryptoMode,
+        AESPaddingMode paddingMode,
+        bool isEncryption,
+        Stream inputStream,
+        Stream outputStream,
+        byte[] key,
+        byte[]? iv = null
+    ) {
+        var keyParameter = ParameterUtilities.CreateKeyParameter("AES", key);
+        var cipher = CipherUtilities.GetCipher($"AES/{cryptoMode}/{paddingMode}");
+        if (iv is null) {
+            cipher.Init(isEncryption, keyParameter);
+        } else {
+            cipher.Init(isEncryption, new ParametersWithIV(keyParameter, iv));
+        }
+        var readBuffer = new byte[ReadBuffer];
+        var writeBuffer = new byte[ReadBuffer];
+        int readCount;
+        inputStream.Position = 0;
+        // processing
+        while ((readCount = inputStream.Read(readBuffer)) > 0) {
+            var writeCount = cipher.ProcessBytes(readBuffer, 0, readCount, writeBuffer, 0);
+            outputStream.Write(writeBuffer, 0, writeCount);
+        }
+        outputStream.Write(cipher.DoFinal());
     }
 }
