9ab8048bc7df0e8cb0d0a11a6667737c1dbde53a
Luxiu123 <klt5466@163.com>
2023-04-12 13:33:15 +0800

    Add ShuffleLinesView text shuffle feature

--- CommonUtil/View/TextTool/ShuffleLinesView.xaml.cs
+++ CommonUtil/View/TextTool/ShuffleLinesView.xaml.cs
@@ -1,7 +1,58 @@
 ﻿namespace CommonUtil.View;
 
 public partial class ShuffleLinesView : Page {
+    public static readonly DependencyProperty OutputTextProperty = DependencyProperty.Register("OutputText", typeof(string), typeof(ShuffleLinesView), new PropertyMetadata(""));
+    public static readonly DependencyProperty InputTextProperty = DependencyProperty.Register("InputText", typeof(string), typeof(ShuffleLinesView), new PropertyMetadata(""));
+
+    /// <summary>
+    /// 输入文本
+    /// </summary>
+    public string InputText {
+        get { return (string)GetValue(InputTextProperty); }
+        set { SetValue(InputTextProperty, value); }
+    }
+    /// <summary>
+    /// 输出文本
+    /// </summary>
+    public string OutputText {
+        get { return (string)GetValue(OutputTextProperty); }
+        set { SetValue(OutputTextProperty, value); }
+    }
+
     public ShuffleLinesView() {
         InitializeComponent();
+    }
+
+    /// <summary>
+    /// 复制结果
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void CopyResultClick(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        Clipboard.SetDataObject(OutputText);
+        MessageBoxUtils.Success("已复制");
+    }
+
+    /// <summary>
+    /// 清空输入
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void ClearInputClick(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        InputText = OutputText = string.Empty;
+    }
+
+    private void TextProcessClickHandler(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        // Empty text
+        if (string.IsNullOrEmpty(InputText)) {
+            MessageBoxUtils.Info("请输入文本");
+            return;
+        }
+        OutputText = string.Join('\n', TextTool.ShuffleLines(
+            InputText.ReplaceLineFeedWithLinuxStyle().Split('\n')
+        ));
     }
 }
