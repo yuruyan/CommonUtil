037d34df928538d95bb933a87fbc84db12e6ae11
Luxiu123 <klt5466@163.com>
2023-04-13 10:27:31 +0800

    Add ShuffleFileLines feature

--- CommonUtil/View/TextTool/ShuffleLinesView.xaml.cs
+++ CommonUtil/View/TextTool/ShuffleLinesView.xaml.cs
@@ -3,6 +3,12 @@
 public partial class ShuffleLinesView : Page {
     public static readonly DependencyProperty OutputTextProperty = DependencyProperty.Register("OutputText", typeof(string), typeof(ShuffleLinesView), new PropertyMetadata(""));
     public static readonly DependencyProperty InputTextProperty = DependencyProperty.Register("InputText", typeof(string), typeof(ShuffleLinesView), new PropertyMetadata(""));
+    public static readonly DependencyProperty FileNameProperty = DependencyProperty.Register("FileName", typeof(string), typeof(ShuffleLinesView), new PropertyMetadata(string.Empty));
+    public static readonly DependencyProperty HasFileProperty = DependencyProperty.Register("HasFile", typeof(bool), typeof(ShuffleLinesView), new PropertyMetadata(false));
+    private readonly SaveFileDialog SaveFileDialog = new() {
+        Title = "保存文件",
+        Filter = "文本文件|*.txt|All Files|*.*"
+    };
 
     /// <summary>
     /// 输入文本
@@ -17,6 +23,20 @@ public partial class ShuffleLinesView : Page {
     public string OutputText {
         get { return (string)GetValue(OutputTextProperty); }
         set { SetValue(OutputTextProperty, value); }
+    }
+    /// <summary>
+    /// 是否有文件
+    /// </summary>
+    public bool HasFile {
+        get { return (bool)GetValue(HasFileProperty); }
+        set { SetValue(HasFileProperty, value); }
+    }
+    /// <summary>
+    /// 文件名
+    /// </summary>
+    public string FileName {
+        get { return (string)GetValue(FileNameProperty); }
+        set { SetValue(FileNameProperty, value); }
     }
 
     public ShuffleLinesView() {
@@ -42,17 +62,71 @@ public partial class ShuffleLinesView : Page {
     private void ClearInputClick(object sender, RoutedEventArgs e) {
         e.Handled = true;
         InputText = OutputText = string.Empty;
+        DragDropTextBox.Clear();
     }
 
-    private void TextProcessClickHandler(object sender, RoutedEventArgs e) {
+    private async void TextProcessClickHandler(object sender, RoutedEventArgs e) {
         e.Handled = true;
-        // Empty text
-        if (string.IsNullOrEmpty(InputText)) {
-            MessageBoxUtils.Info("请输入文本");
+        // 输入检查
+        if (!await UIUtils.CheckTextAndFileInputAsync(InputText, HasFile, FileName)) {
             return;
         }
+
+        // 文本处理
+        if (!HasFile) {
+            StringTextProcess();
+            return;
+        }
+        ThrottleUtils.ThrottleAsync(
+            $"{nameof(SortLinesView)}|{nameof(TextProcessClickHandler)}|{GetHashCode()}",
+            FileTextProcess
+        );
+    }
+
+    /// <summary>
+    /// 文本处理
+    /// </summary>
+    private void StringTextProcess() {
         OutputText = string.Join('\n', TextTool.ShuffleLines(
             InputText.ReplaceLineFeedWithLinuxStyle().Split('\n')
         ));
+    }
+
+    /// <summary>
+    /// 文件文本处理
+    /// </summary>
+    private async Task FileTextProcess() {
+        var text = InputText;
+        var inputPath = FileName;
+        if (SaveFileDialog.ShowDialog() != true) {
+            return;
+        }
+        var outputPath = SaveFileDialog.FileName;
+
+        // 处理
+        await UIUtils.CreateFileProcessTask(
+            TextTool.FileShuffleLines,
+            outputPath,
+            args: new object[] { inputPath, outputPath }
+        );
+    }
+
+    /// <summary>
+    /// DragDropEvent
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void DragDropEventHandler(object sender, object e) {
+        if (e is IEnumerable<string> array) {
+            if (!array.Any()) {
+                return;
+            }
+            // 判断是否为文件
+            if (File.Exists(array.First())) {
+                FileName = array.First();
+            } else {
+                DragDropTextBox.Clear();
+            }
+        }
     }
 }
