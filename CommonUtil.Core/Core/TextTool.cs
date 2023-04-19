02ae5162091b4cf1d0db73ef1636ad2bfa1c7273
Luxiu123 <klt5466@163.com>
2023-04-19 23:04:41 +0800

    Add PunctuationReplace feature

--- CommonUtil.Core/Core/TextTool.cs
+++ CommonUtil.Core/Core/TextTool.cs
@@ -46,6 +46,39 @@ public static partial class TextTool {
     /// 全角半角 Dict
     /// </summary>
     private static readonly IReadOnlyDictionary<char, char> FullHalfCharDict;
+    /// <summary>
+    /// 英文中文标点符号
+    /// </summary>
+    private static readonly IReadOnlyDictionary<char, char> EnglishChinesePunctuationDict = new Dictionary<char, char>() {
+        {'!', '！'},
+        {'(', '（'},
+        {')', '）'},
+        {'\'', '\''},
+        {'"', '"'},
+        {';', '；'},
+        {':', '：'},
+        {',', '，'},
+        {'.', '。'},
+        {'?', '？'},
+    };
+    /// <summary>
+    /// 中文英文标点符号
+    /// </summary>
+    private static readonly IReadOnlyDictionary<char, char> ChineseEnglishPunctuationDict = new Dictionary<char, char>() {
+        {'！', '!'},
+        {'（', '('},
+        {'）', ')'},
+        {'、', ','},
+        {'‘', '\''},
+        {'’', '\''},
+        {'“', '"'},
+        {'”', '"'},
+        {'；', ';'},
+        {'：', ':'},
+        {'，', ','},
+        {'。', '.'},
+        {'？', '?'},
+    };
     #endregion
 
 #if NET7_0_OR_GREATER
@@ -365,6 +398,36 @@ public static partial class TextTool {
         }
         return data;
     }
+
+    /// <summary>
+    /// 替换英文标点为中文标点
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    public static string ReplaceEnglishPunctuationWithChinese(string text) {
+        var array = text.ToCharArray();
+        for (int i = 0; i < array.Length; i++) {
+            if (EnglishChinesePunctuationDict.TryGetValue(array[i], out var ch)) {
+                array[i] = ch;
+            }
+        }
+        return new(array);
+    }
+
+    /// <summary>
+    /// 替换中文标点为英文标点
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    public static string ReplaceChinesePunctuationWithEnglish(string text) {
+        var array = text.ToCharArray();
+        for (int i = 0; i < array.Length; i++) {
+            if (ChineseEnglishPunctuationDict.TryGetValue(array[i], out var ch)) {
+                array[i] = ch;
+            }
+        }
+        return new(array);
+    }
     #endregion
 
     #region 文件处理
@@ -581,6 +644,24 @@ public static partial class TextTool {
     /// <param name="outputPath"></param>
     public static void FileShuffleLines(string inputPath, string outputPath) {
         File.WriteAllLines(outputPath, ShuffleLines(File.ReadAllLines(inputPath)));
+    }
+
+    /// <summary>
+    /// 文件替换英文标点为中文标点
+    /// </summary>
+    /// <param name="inputPath"></param>
+    /// <param name="outputPath"></param>
+    public static void FileReplaceEnglishPunctuationWithChinese(string inputPath, string outputPath) {
+        ProcessFileText(inputPath, outputPath, ReplaceEnglishPunctuationWithChinese);
+    }
+
+    /// <summary>
+    /// 文件替换中文标点为英文标点
+    /// </summary>
+    /// <param name="inputPath"></param>
+    /// <param name="outputPath"></param>
+    public static void FileReplaceChinesePunctuationWithEnglish(string inputPath, string outputPath) {
+        ProcessFileText(inputPath, outputPath, ReplaceChinesePunctuationWithEnglish);
     }
     #endregion
 }
