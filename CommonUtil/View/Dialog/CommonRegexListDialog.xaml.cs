1ba90a03134c3b6b095c2b0510ee4b149037adc8
Luxiu123 <klt5466@163.com>
2023-04-21 16:12:02 +0800

    Refactor CommonRegexListDialog

--- CommonUtil/View/Dialog/CommonRegexListDialog.xaml.cs
+++ CommonUtil/View/Dialog/CommonRegexListDialog.xaml.cs
@@ -1,35 +1,22 @@
 ﻿using CommonUtil.Data;
+using CommonUtil.Data.Resources;
 
 namespace CommonUtil.View;
 
 public partial class CommonRegexListDialog : BaseDialog {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-    public static readonly DependencyProperty RegexListProperty = DependencyProperty.Register("RegexList", typeof(IEnumerable<KeyValuePair<string, string>>), typeof(CommonRegexListDialog), new PropertyMetadata());
-    private static readonly IEnumerable<KeyValuePair<string, string>> CommonRegexList;
+    public static readonly DependencyProperty RegexListProperty = DependencyProperty.Register("RegexList", typeof(IList<KeyValuePair<string, string>>), typeof(CommonRegexListDialog), new PropertyMetadata());
 
     /// <summary>
     /// 正则列表
     /// </summary>
-    public IEnumerable<KeyValuePair<string, string>> RegexList {
-        get { return (IEnumerable<KeyValuePair<string, string>>)GetValue(RegexListProperty); }
+    public IList<KeyValuePair<string, string>> RegexList {
+        get { return (IList<KeyValuePair<string, string>>)GetValue(RegexListProperty); }
         set { SetValue(RegexListProperty, value); }
     }
 
-    /// <summary>
-    /// 加载 CommonRegexList
-    /// </summary>
-    static CommonRegexListDialog() {
-        var list = JsonConvert.DeserializeObject<IEnumerable<KeyValuePair<string, string>>>(
-            Encoding.UTF8.GetString(DataResource.CommonRegex)
-        );
-        if (list == null) {
-            throw new JsonSerializationException("解析 CommonRegexList 失败");
-        }
-        CommonRegexList = list;
-    }
-
     public CommonRegexListDialog() {
-        RegexList = CommonRegexList;
+        RegexList = new List<KeyValuePair<string, string>>(DataResourceHelper.CommonRegexList);
         InitializeComponent();
         this.EnableAutoResize(
             (double)Resources["RegexListItemsControlMinWidth"],
