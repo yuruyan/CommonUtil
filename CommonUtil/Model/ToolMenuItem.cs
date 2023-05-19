5912f58fcf9dab3adc58d92308d51869f932a71b
Luxiu123 <klt5466@163.com>
2023-05-19 16:28:02 +0800

    Refactor ToolMenuItem struct

--- CommonUtil/Model/ToolMenuItem.cs
+++ CommonUtil/Model/ToolMenuItem.cs
@@ -3,11 +3,13 @@
 namespace CommonUtil.Model;
 
 public readonly record struct ToolMenuItem {
+    public string Id { get; }
     public string Name { get; }
     public string ImagePath { get; }
     public Type ClassType { get; }
 
-    public ToolMenuItem(string name, string imagePath, Type classType) {
+    public ToolMenuItem(string id, string name, string imagePath, Type classType) {
+        Id = id;
         Name = name;
         ImagePath = imagePath;
         ClassType = classType;
@@ -18,6 +20,7 @@ public class ToolMenuItemDO : DependencyObject {
     public static readonly DependencyProperty NameProperty = DependencyProperty.Register("Name", typeof(string), typeof(ToolMenuItemDO), new PropertyMetadata(string.Empty));
     public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(string), typeof(ToolMenuItemDO), new PropertyMetadata());
 
+    public string Id { get; }
     public Type ViewType { get; }
     /// <summary>
     /// 菜单名称
@@ -34,7 +37,8 @@ public class ToolMenuItemDO : DependencyObject {
         set { SetValue(IconProperty, value); }
     }
 
-    public ToolMenuItemDO(Type viewType) {
+    public ToolMenuItemDO(string id, Type viewType) {
+        Id = id;
         ViewType = viewType;
     }
 }
@@ -43,7 +47,7 @@ public class ToolMenuItemProfile : Profile {
     public ToolMenuItemProfile() {
         CreateMap<ToolMenuItem, ToolMenuItem>();
         CreateMap<ToolMenuItem, ToolMenuItemDO>()
-            .ConstructUsing(src => new ToolMenuItemDO(src.ClassType))
+            .ConstructUsing(src => new ToolMenuItemDO(src.Id, src.ClassType))
             .ForMember(dest => dest.Icon, cfg => cfg.MapFrom(src => src.ImagePath));
         CreateMap<ToolMenuItemDO, ToolMenuItemDO>();
         CreateMap<ToolMenuItemDO, ToolMenuItem>()
