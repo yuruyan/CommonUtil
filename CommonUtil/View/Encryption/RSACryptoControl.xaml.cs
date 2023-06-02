703d7bb597be3be9567ea6806446d07cd3456bf6
Luxiu123 <klt5466@163.com>
2023-06-02 19:24:03 +0800

    Add RSACryptoControl

---
+++ CommonUtil/View/Encryption/RSACryptoControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace CommonUtil.View;
+
+public partial class RSACryptoControl : ResponsiveUserControl {
+    public static readonly DependencyProperty SelectedAlgorithmProperty = DependencyProperty.Register("SelectedAlgorithm", typeof(string), typeof(RSACryptoControl), new PropertyMetadata());
+    public static readonly DependencyProperty IsWorkingProperty = DependencyProperty.Register("IsWorking", typeof(bool), typeof(RSACryptoControl), new PropertyMetadata(false));
+
+    public event RoutedEventHandler? EncryptClick;
+    public event RoutedEventHandler? DecryptClick;
+    public event RoutedEventHandler? CopyResultClick;
+    public event RoutedEventHandler? ClearInputClick;
+
+    public string SelectedAlgorithm {
+        get { return (string)GetValue(SelectedAlgorithmProperty); }
+        set { SetValue(SelectedAlgorithmProperty, value); }
+    }
+    public bool IsWorking {
+        get { return (bool)GetValue(IsWorkingProperty); }
+        set { SetValue(IsWorkingProperty, value); }
+    }
+
+    public RSACryptoControl() : base(ResponsiveMode.Variable) {
+        InitializeComponent();
+    }
+
+    private void EncryptClickHandler(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        EncryptClick?.Invoke(sender, e);
+    }
+
+    private void DecryptClickHandler(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        DecryptClick?.Invoke(sender, e);
+    }
+
+    private void CopyResultClickHandler(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        CopyResultClick?.Invoke(sender, e);
+    }
+
+    private void ClearInputClickHandler(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        ClearInputClick?.Invoke(sender, e);
+    }
+}
