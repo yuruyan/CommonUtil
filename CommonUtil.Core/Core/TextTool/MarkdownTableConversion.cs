82ff66fb96da5de48ae4eef34c88a7e09899684d
Luxiu123 <klt5466@163.com>
2023-08-20 19:42:23 +0800

    Add MarkdownTableConversion

---
+++ CommonUtil.Core/Core/TextTool/MarkdownTableConversion.cs
@@ -0,0 +1,37 @@
+﻿namespace CommonUtil.Core;
+
+public static class MarkdownTableConversion {
+    /// <summary>
+    /// 转换为 Markdown 表格
+    /// </summary>
+    /// <param name="text"></param>
+    /// <param name="separator">列分隔符</param>
+    /// <returns></returns>
+    public static string ConvertToTable(string text, string separator) {
+        var rows = text.ReplaceLineFeedWithLinuxStyle().Split('\n');
+        var newRows = new List<string>(rows.Length);
+        var splitList = rows
+            .Where(row => !string.IsNullOrWhiteSpace(row))
+            .Select(row => row.Split(separator, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+            .ToList();
+        var maxColumnCount = splitList.Max(line => line.Length);
+        foreach (var row in splitList) {
+            // 补足长度
+            var content = string.Join(
+                '|',
+                row.ToList().ResizeToLength(maxColumnCount, string.Empty)
+            );
+            newRows.Add($"|{content}|");
+        }
+        // Add seperator between header and content
+        if (newRows.Count > 1) {
+            var content = string.Join(
+                '|',
+                new List<string>(0)
+                    .ResizeByCount(maxColumnCount, "-")
+            );
+            newRows.Insert(1, $"|{content}|");
+        }
+        return string.Join('\n', newRows);
+    }
+}
