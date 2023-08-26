96ebe3a48e2cb8c473add3788b3b5d73ab14b574
Luxiu123 <klt5466@163.com>
2023-08-26 22:25:46 +0800

    Add UploadFilesAsync api

--- SimpleFileSystemServer/SimpleFileSystemServer/Controllers/HomeController.cs
+++ SimpleFileSystemServer/SimpleFileSystemServer/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CommonTools.Utils;
+using Microsoft.AspNetCore.Mvc;
 using SimpleFileSystemServer.Service;
+using System.Text.RegularExpressions;
 
 namespace SimpleFileSystemServer.Controllers;
 
 [Route("")]
 public class HomeController : Controller {
+    private static readonly Regex InvalidFileNameCharsRegex = new($@"[{string.Join("", Path.GetInvalidFileNameChars())}]");
+
     /// <summary>
     /// 列出文件列表
     /// </summary>
     /// <param name="dir">文件夹路径，以 '/' 开头</param>
     /// <returns></returns>
-    [HttpGet("list")]
+    [HttpGet("/list")]
     public JsonResponse<IEnumerable<FileVO>> ListFiles([FromQuery] string dir = "/") {
         dir = PathUtils.Normalize(dir);
         // 非法访问
@@ -34,8 +38,37 @@ public class HomeController : Controller {
     /// favicon
     /// </summary>
     /// <returns></returns>
-    [HttpGet("favicon.ico")]
+    [HttpGet("/favicon.ico")]
     public FileResult Favicon() => File(System.IO.File.OpenRead("favicon.ico"), "image/x-icon");
+
+    /// <summary>
+    /// 下载文件
+    /// </summary>
+    /// <param name="files"></param>
+    /// <returns></returns>
+    [HttpPost("/upload")]
+    [RequestFormLimits(MultipartBodyLengthLimit = ConstantUtils.OneGbSize << 6)]
+    [RequestSizeLimit(ConstantUtils.OneGbSize << 6)]
+    public async Task<JsonResponse> UploadFilesAsync([FromForm(Name = "dir")] string dir, [FromForm(Name = "file")] IEnumerable<IFormFile> files) {
+        dir = PathUtils.Normalize(dir);
+        // Invalid path
+        if (!PathUtils.CheckPathRange(dir)) {
+            return JsonResponse.Error;
+        }
+
+        var absDirPath = PathUtils.GetAbsolutePath(dir);
+        foreach (var formFile in files) {
+            // 检查文件名合法性
+            var fileName = InvalidFileNameCharsRegex.Replace(formFile.FileName, "");
+            var filePath = Path.Combine(absDirPath, fileName);
+            // 检查文件是否重复
+            filePath = CommonUtils.GetUniqueFileNameFor(filePath);
+            await Console.Out.WriteLineAsync(filePath);
+            using var stream = System.IO.File.OpenWrite(filePath);
+            await TaskUtils.TryAsync(() => formFile.CopyToAsync(stream));
+        }
+        return JsonResponse.Success;
+    }
 
     /// <summary>
     /// 下载文件
