22d6ebd5fd01093fb69336f1e4c1181190b19ad1
Luxiu123 <13467486215@163.com>
2023-11-30 18:45:19 +0800

    Add LengthPartitionTests

---
+++ CommonUtil.CoreTests/Core/TextTool/LengthPartitionTests.cs
@@ -0,0 +1,14 @@
+﻿using CommonTools.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonUtil.Core.Tests;
+
+[TestClass()]
+public class LengthPartitionTests {
+    [TestMethod()]
+    public void SplitTest() {
+        var list = LengthPartition.Split("hell", 2);
+        Console.WriteLine(list.Count);
+        list.Print();
+    }
+}
