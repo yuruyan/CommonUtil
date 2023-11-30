81a573a5be77a788a26d3f35a3004f6fb58e14c8
Luxiu123 <13467486215@163.com>
2023-11-30 18:45:10 +0800

    Add LengthPartition

---
+++ CommonUtil.Core/Core/TextTool/LengthPartition.cs
@@ -0,0 +1,23 @@
+﻿namespace CommonUtil.Core;
+
+/// <summary>
+/// 根据长度分割字符串
+/// </summary>
+public static class LengthPartition {
+    public static List<string> Split(string text, int length) {
+        if (length <= 0) {
+            throw new ArgumentOutOfRangeException(nameof(length));
+        }
+
+        var textLength = text.Length;
+        var results = new List<string>(textLength / length);
+        int i = 0;
+        for (i = 0; i + length < textLength; i += length) {
+            results.Add(text[i..(i + length)]);
+        }
+        if (i < textLength) {
+            results.Add(text[i..]);
+        }
+        return results;
+    }
+}
