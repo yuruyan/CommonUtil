a3f49a14a19a46a97baf4916c394ada6115dab7f
Luxiu123 <13467486215@163.com>
2023-12-09 13:40:13 +0800

    Rename BaseNumberStringConverter parameter name

--- CommonUtil.Core/Core/CommonEncoding/BaseNumberStringConverter.cs
+++ CommonUtil.Core/Core/CommonEncoding/BaseNumberStringConverter.cs
@@ -10,24 +10,24 @@ static class BaseNumberStringConverter {
     /// <summary>
     /// 字符串转 8 进制数字
     /// </summary>
-    /// <param name="value">字符串</param>
+    /// <param name="text">字符串</param>
     /// <param name="padding">是否补全左侧为 0</param>
     /// <returns></returns>
     /// <exception cref="FormatException">字符串中含有超过 <see cref="MaxOctalNumber"/> 的字符</exception>
-    public static string ConvertToOctalNumber(string value, bool padding = false) {
-        if (!CheckCharacterInRange(value, MaxOctalNumber)) {
+    public static string ConvertToOctalNumber(string text, bool padding = false) {
+        if (!CheckCharacterInRange(text, MaxOctalNumber)) {
             throw new FormatException("Invalid string conversion");
         }
 
-        var sb = new StringBuilder(value.Length << 2);
+        var sb = new StringBuilder(text.Length << 2);
         if (padding) {
-            foreach (char c in value) {
+            foreach (char c in text) {
                 sb.Append('\\').Append(
                     Convert.ToString(c, 8).PadLeft(3, '0')
                 );
             }
         } else {
-            foreach (char c in value) {
+            foreach (char c in text) {
                 sb.Append('\\').Append(Convert.ToString(c, 8));
             }
         }
@@ -37,24 +37,24 @@ static class BaseNumberStringConverter {
     /// <summary>
     /// ASCII 字符串转 16 进制数字
     /// </summary>
-    /// <param name="value">ASCII 字符串</param>
+    /// <param name="text">ASCII 字符串</param>
     /// <param name="padding">是否补全左侧为 0</param>
     /// <returns></returns>
     /// <exception cref="FormatException">字符串中含有超过 <see cref="MaxHexidecimalASCIINumber"/> 的字符</exception>
-    public static string ConvertToHexASCIINumber(string value, bool padding = false) {
-        if (!CheckCharacterInRange(value, MaxHexidecimalASCIINumber)) {
+    public static string ConvertToHexASCIINumber(string text, bool padding = false) {
+        if (!CheckCharacterInRange(text, MaxHexidecimalASCIINumber)) {
             throw new FormatException("Invalid string conversion");
         }
 
-        var sb = new StringBuilder(value.Length << 2);
+        var sb = new StringBuilder(text.Length << 2);
         if (padding) {
-            foreach (char c in value) {
+            foreach (char c in text) {
                 sb.Append("\\x").Append(
                     Convert.ToString(c, 16).PadLeft(2, '0')
                 );
             }
         } else {
-            foreach (char c in value) {
+            foreach (char c in text) {
                 sb.Append("\\x").Append(Convert.ToString(c, 16));
             }
         }
@@ -64,19 +64,19 @@ static class BaseNumberStringConverter {
     /// <summary>
     /// Unicode 字符串转 16 进制数字
     /// </summary>
-    /// <param name="value">Unicode 字符串</param>
+    /// <param name="text">Unicode 字符串</param>
     /// <param name="padding">是否补全左侧为 0</param>
     /// <returns></returns>
-    public static string ConvertToHexUnicodeNumber(string value, bool padding = false) {
-        var sb = new StringBuilder(value.Length * 6);
+    public static string ConvertToHexUnicodeNumber(string text, bool padding = false) {
+        var sb = new StringBuilder(text.Length * 6);
         if (padding) {
-            foreach (char c in value) {
+            foreach (char c in text) {
                 sb.Append("\\u").Append(
                     Convert.ToString(c, 16).PadLeft(4, '0')
                 );
             }
         } else {
-            foreach (char c in value) {
+            foreach (char c in text) {
                 sb.Append("\\u").Append(Convert.ToString(c, 16));
             }
         }
@@ -86,12 +86,12 @@ static class BaseNumberStringConverter {
     /// <summary>
     /// FullUnicode 字符串转 16 进制数字
     /// </summary>
-    /// <param name="value">FullUnicode 字符串</param>
+    /// <param name="text">FullUnicode 字符串</param>
     /// <param name="padding">是否补全左侧为 0</param>
     /// <returns></returns>
-    public static string ConvertToHexFullUnicodeNumber(string value, bool padding = false) {
-        var sb = new StringBuilder(value.Length * 10);
-        var data = Encoding.UTF32.GetBytes(value);
+    public static string ConvertToHexFullUnicodeNumber(string text, bool padding = false) {
+        var sb = new StringBuilder(text.Length * 10);
+        var data = Encoding.UTF32.GetBytes(text);
         if (padding) {
             for (int i = 0; i < data.Length; i += 4) {
                 sb.Append("\\U").Append(
