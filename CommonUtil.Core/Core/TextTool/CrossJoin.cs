9c9466b914e2e3e1d633bc885c55051649df92a5
Luxiu123 <13467486215@163.com>
2023-12-13 15:25:51 +0800

    Add CrossJoin

---
+++ CommonUtil.Core/Core/TextTool/CrossJoin.cs
@@ -0,0 +1,20 @@
+﻿namespace CommonUtil.Core;
+
+/// <summary>
+/// 笛卡尔积
+/// </summary>
+public static class CrossJoin {
+    public static List<string> Join(IEnumerable<IEnumerable<string>> data) {
+        var results = new List<string>() { string.Empty };
+        foreach (var list in data) {
+            var copyList = results.ToList();
+            results.Clear();
+            foreach (var item1 in copyList) {
+                foreach (var item2 in list) {
+                    results.Add(item1 + item2);
+                }
+            }
+        }
+        return results;
+    }
+}
