4dade1c83d475cc2a04ea866a2176acd4b1bcfff
Luxiu123 <13467486215@163.com>
2023-12-19 18:40:07 +0800

    Add DictionaryReplacement

---
+++ CommonUtil.Core/Core/TextTool/DictionaryReplacement.cs
@@ -0,0 +1,17 @@
+﻿namespace CommonUtil.Core;
+
+/// <summary>
+/// 字典替换文本
+/// </summary>
+public static class DictionaryReplacement {
+    public static string ReplaceAggregate(string text, IReadOnlyDictionary<string, string> replacement) {
+        foreach (var dict in replacement) {
+            text = text.Replace(dict.Key, dict.Value);
+        }
+        return text;
+    }
+
+    public static void FileReplaceAggregate(string inputPath, string outputPath, IReadOnlyDictionary<string, string> replacement) {
+        File.WriteAllText(outputPath, ReplaceAggregate(File.ReadAllText(inputPath), replacement));
+    }
+}
