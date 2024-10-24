a25a130ac1e1509503dafa89438a6ae25122b2ef
yuruyan <luxiu.klt@foxmail.com>
2024-10-24 16:06:46 +0800

    Add HtmlTableConversion

---
+++ CommonUtil.Core/Core/TextTool/HtmlTableConversion.cs
@@ -0,0 +1,51 @@
+﻿namespace CommonUtil.Core;
+
+public static class HtmlTableConversion {
+    public static string ConvertToTable(string text, string separator) {
+        var rows = text.ReplaceLineFeedWithLinuxStyle().Split('\n');
+        var newRows = new List<string>(rows.Length);
+        var splitList = rows
+            .Where(row => !string.IsNullOrWhiteSpace(row))
+            .Select(row => row.Split(separator, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+            .ToList();
+        var maxColumnCount = splitList.Max(line => line.Length);
+        // 统一列数
+        splitList = splitList.Select(
+            line => line
+                .Select(EscapeCharacters)
+                .ToList()
+                .ResizeToLength(maxColumnCount, string.Empty)
+                .ToArray()
+        ).ToList();
+        var sb = new StringBuilder();
+        sb.Append("<table>").Append("<thead>").Append("<tr>");
+        for (int i = 0; i < maxColumnCount; i++) {
+            sb.Append("<th>").Append(splitList[0][i]).Append("</th>");
+        }
+        sb.Append("</tr>").Append("</thead>").Append("<tbody>");
+
+        for (int i = 1; i < splitList.Count; i++) {
+            sb.Append("<tr>");
+            foreach (var item in splitList[i]) {
+                sb.Append("<td>").Append(item).Append("</td>");
+            }
+            sb.Append("</tr>");
+        }
+        sb.Append("</tbody>").Append("</table>");
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// 转义字符
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    private static string EscapeCharacters(string text) {
+        return text
+            .Replace("<", "&lt;")
+            .Replace(">", "&gt;")
+            .Replace("&", "&amp;")
+            .Replace("\"", "&quot;")
+            .Replace("'", "&apos;");
+    }
+}
---
+++ CommonUtil/View/TextTool/HtmlTableConversionView.xaml.cs
@@ -0,0 +1,88 @@
+﻿namespace CommonUtil.View;
+
+public partial class HtmlTableConversionView : ResponsivePage {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+    private readonly IDictionary<string, string> SplitTextDict;
+    public static readonly DependencyProperty OutputTextProperty = DependencyProperty.Register("OutputText", typeof(string), typeof(HtmlTableConversionView), new PropertyMetadata(string.Empty));
+    public static readonly DependencyProperty InputTextProperty = DependencyProperty.Register("InputText", typeof(string), typeof(HtmlTableConversionView), new PropertyMetadata(string.Empty));
+    public static readonly DependencyProperty SplitTextOptionsProperty = DependencyProperty.Register("SplitTextOptions", typeof(List<string>), typeof(HtmlTableConversionView), new PropertyMetadata());
+
+    /// <summary>
+    /// 输入文本
+    /// </summary>
+    public string InputText {
+        get { return (string)GetValue(InputTextProperty); }
+        set { SetValue(InputTextProperty, value); }
+    }
+    /// <summary>
+    /// 输出文本
+    /// </summary>
+    public string OutputText {
+        get { return (string)GetValue(OutputTextProperty); }
+        set { SetValue(OutputTextProperty, value); }
+    }
+    /// <summary>
+    /// 分隔文本选项
+    /// </summary>
+    public List<string> SplitTextOptions {
+        get { return (List<string>)GetValue(SplitTextOptionsProperty); }
+        set { SetValue(SplitTextOptionsProperty, value); }
+    }
+
+    public HtmlTableConversionView() : base(ResponsiveMode.Variable) {
+        SplitTextDict = new Dictionary<string, string>(DataSet.MarkdownTableConversionSplitOptionDict);
+        SplitTextOptions = new(SplitTextDict.Keys);
+        InitializeComponent();
+    }
+
+    /// <summary>
+    /// 复制结果
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void CopyResultClick(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        Clipboard.SetDataObject(OutputText);
+        MessageBoxUtils.Success("已复制");
+    }
+
+    /// <summary>
+    /// 清空输入
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void ClearInputClick(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        InputText = OutputText = string.Empty;
+    }
+
+    /// <summary>
+    /// 生成表格
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void GenerateTablerClick(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        if (string.IsNullOrEmpty(InputText)) {
+            MessageBoxUtils.Info("请输入文本");
+            return;
+        }
+        OutputText = HtmlTableConversion.ConvertToTable(InputText, GetComboBoxText());
+    }
+
+    /// <summary>
+    /// 获取 ComboBox 文本
+    /// </summary>
+    /// <returns></returns>
+    private string GetComboBoxText() {
+        object selectedValue = SplitTextComboBox.SelectedValue;
+        string text = SplitTextComboBox.Text;
+        // 非用户输入
+        if (selectedValue != null) {
+            if (selectedValue is string t) {
+                text = SplitTextDict[t];
+            }
+        }
+        return text;
+    }
+}
